35117cf8e0eb5ca1efcf2501f1429701a391d51c
Matthieu Faessel <faessel@cmm.ensmp.fr>
2012-08-03 22:16:06 +0200

    Modif demo benchs

--- doc/demos/python/benchs.py
+++ doc/demos/python/benchs.py
@@ -72,10 +72,22 @@ im3 = Image(im1)
 copy(imIn, tmpIm)
 resize(tmpIm, im1)
 
+print "*** Base ***"
+bench(copy, im1, im2)
 bench(fill,im1, 0)
 bench(inv, im1, im2)
-bench(sup, im1, im2, im3)
+bench(add, im1, im2, im3)
+bench(sub, im1, im2, im3)
+bench(mul, im1, im2, im3)
+bench(div, im1, im2, im3)
 
+print "\n*** Arithmetic ***"
+bench(inf, im1, im2, im3)
+bench(sup, im1, im2, im3)
+bench(equ, im1, im2, im3)
+bench(low, im1, im2, im3)
+
+print "\n*** Morphology ***"
 bench(dilate, im1, im2, hSE(1))
 bench(dilate, im1, im2, sSE(1))
 bench(erode, im1, im2, hSE(1))
