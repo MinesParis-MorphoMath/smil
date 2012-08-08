ef093baa64f83ecaa5343d4230775c74b1e1de1b
Matthieu Faessel <faessel@cmm.ensmp.fr>
2012-08-08 19:29:52 +0200

    Modif demo crop.

--- doc/demos/python/copy_crop.py
+++ doc/demos/python/copy_crop.py
@@ -2,14 +2,14 @@ from smilPython import *
 
 im1 = Image("http://cmm.ensmp.fr/~faessel/smil/images/barbara.png")
 im2 = Image("http://cmm.ensmp.fr/~faessel/smil/images/lena.png")
-im3 = Image(im2)
+im3 = Image(im1)
 
 im1.show()
 im2.show()
 im3.show()
 
-# Copy the content of im1 from (256, 0) in im3
-copy(im1, 256, 0, im3)
+# Crop the content of im1 from (256, 0) to im3 (which will be resized)
+crop(im1, 256, 0, 256, 256, im3)
 
 # Copy the content of im2 and put it at position (0, 256) in im1
 copy(im2, im1, 0, 256)
@@ -20,9 +20,9 @@ copy(im1, 256, 0, 128, 128, im2, 128, 128)
 copy(im1, 256, 0, im2, 128, 128)
 
 # Create a 3D image and copy slices inside
-im3D = Image(im1.getWidth(), im1.getHeight(), 4)
+im3D = Image(im2.getWidth(), im2.getHeight(), 3)
 im3D << 0
-copy(im1, im3D)
-copy(im2, im3D, 128, 128, 1)
-copy(im3, im3D, 128, 128, 2)
+copy(im1, 0, 256, im3D)
+copy(im3, im3D, 0, 0, 2)
+close(im3D, im3D, cubeSE())
 im3D.show()
