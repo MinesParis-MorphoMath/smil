a118cfd95dcab71b1483de8f3178c430f1197fa7
Matthieu Faessel <faessel@cmm.ensmp.fr>
2013-11-18 23:27:22 +0100

    Modif interactive_segmentation demo

--- doc/demos/python/interactive_segmentation.py
+++ doc/demos/python/interactive_segmentation.py
@@ -1,9 +1,4 @@
-
 from smilPython import *
-
-class slot(EventSlot):
-    def run(self, event):
-        watershed(im2, v.getOverlay(), im3, im4)
 
 
 im1 = Image("http://cmm.ensmp.fr/~faessel/smil/images/tools.png")
@@ -17,13 +12,17 @@ im1.show()
 im3.show()
 im4.showLabel()
 
-v = im1.getViewer()
-
+class slot(EventSlot):
+    def run(self, event):
+        watershed(im2, v.getOverlay(), im3, im4)
 s = slot()
-v.onOverlayModified.connect(s)
-im1.show()
 
-print "Now, right click on im1, in the \"Tools\" menu select \"Draw\", and draw markers (with different colors) on image."
+v = im1.getViewer()
+v.onOverlayModified.connect(s)
+
+print "1) Right click on im1"
+print "2) In the \"Tools\" menu select \"Draw\""
+print "3) Draw markers (with different colors) on im1 and view the resulting segmentation"
 
 # Will crash if not in a Qt loop
 Gui.execLoop()
