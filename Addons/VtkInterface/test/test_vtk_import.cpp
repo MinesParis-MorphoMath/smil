c8381e46e8429c5f66da7ad29bb68190ca35e85a
Matthieu Faessel <faessel@cmm.ensmp.fr>
2014-01-28 16:11:59 +0100

    Add vtkImageData interface

---
+++ Addons/VtkInterface/test/test_vtk_import.cpp
@@ -0,0 +1,72 @@
+
+#ifdef WRAP_PYTHON
+    #include <boost/python.hpp>
+#endif // WRAP_PYTHON
+
+#include "DVtkInterface.hpp"
+#include "DTest.h"
+
+
+using namespace smil;
+
+class Test_Import : public TestCase
+{
+    virtual void run()
+    {
+	vtkImageData *imData = vtkImageData::New();
+	imData->SetExtent(0, 49, 0, 49, 0, 49);
+	imData->SetScalarTypeToUnsignedShort();
+	imData->SetNumberOfScalarComponents(1); // image holds one value intensities
+	imData->AllocateScalars(); // allocate
+	
+	VtkInt<UINT8> dIm(imData);
+	dIm.show();
+	Gui::execLoop();
+	
+	imData->Delete();
+    }
+};
+
+#ifdef WRAP_PYTHON
+    class Test_Python_Import : public TestCase
+    {
+	virtual void run()
+	{
+// 	  Py_Initialize();
+// 
+// 	  PyObject *_main = PyImport_ImportModule("__main__");
+// 	  PyObject *globals = PyModule_GetDict(_main);
+// 	  
+// 	  PyRun_String("import sys", Py_file_input, globals, NULL);
+// 	  PyRun_SimpleString((string("sys.path.append(\"") + MORPHEE_LIBRARY_DIR + "\")").c_str());
+// 	  PyRun_SimpleString("import MorpheePython as mp");
+// 	  PyRun_SimpleString("mIm = mp.createImage(mp.dataCategory.dtScalar, mp.scalarDataType.sdtUINT8)");
+// 	  PyRun_SimpleString("mIm.setSize(256,256)");
+// 	  PyRun_SimpleString("mIm.setColorInfo(mp.colorInfo.ciMonoSpectral)");
+// 	  PyRun_SimpleString("mIm.allocateImage()");
+// 
+// 	  PyObject *pyobj = PyDict_GetItem(globals, PyString_FromString( "mIm" ));
+// 
+// 	  MorphmInt<UINT8> mIm(pyobj);
+// 	  TEST_ASSERT(mIm.isAllocated());
+// 	  
+// 	  Py_Finalize();
+	    
+	}
+    };
+#endif // WRAP_PYTHON
+
+int main(int argc, char *argv[])
+{
+      TestSuite ts;
+      
+      ADD_TEST(ts, Test_Import);
+      
+#ifdef WRAP_PYTHON
+      ADD_TEST(ts, Test_Python_Import);
+#endif // WRAP_PYTHON
+      
+      return ts.run();
+  
+}
+
