b7b20b1154fbe95dd8aad0c7c3fb5e972b832546
Matthieu Faessel <faessel@cmm.ensmp.fr>
2014-01-29 11:43:21 +0100

    Misc Addons

--- Addons/MorphM/test/test_morphm_import.cpp
+++ Addons/MorphM/test/test_morphm_import.cpp
@@ -1,8 +1,4 @@
 
-#ifdef WRAP_PYTHON
-    #include <boost/python.hpp>
-#endif // WRAP_PYTHON
-
 #include "DMorphMImage.hpp"
 #include "DTest.h"
 
@@ -10,42 +6,10 @@
 using namespace smil;
 
 
-#ifdef WRAP_PYTHON
-    class Test_Python_Import : public TestCase
-    {
-	virtual void run()
-	{
-	  Py_Initialize();
-
-	  PyObject *_main = PyImport_ImportModule("__main__");
-	  PyObject *globals = PyModule_GetDict(_main);
-	  
-	  PyRun_String("import sys", Py_file_input, globals, NULL);
-	  PyRun_SimpleString((string("sys.path.append(\"") + MORPHEE_LIBRARY_DIR + "\")").c_str());
-	  PyRun_SimpleString("import MorpheePython as mp");
-	  PyRun_SimpleString("mIm = mp.createImage(mp.dataCategory.dtScalar, mp.scalarDataType.sdtUINT8)");
-	  PyRun_SimpleString("mIm.setSize(256,256)");
-	  PyRun_SimpleString("mIm.setColorInfo(mp.colorInfo.ciMonoSpectral)");
-	  PyRun_SimpleString("mIm.allocateImage()");
-
-	  PyObject *pyobj = PyDict_GetItem(globals, PyString_FromString( "mIm" ));
-
-	  MorphmInt<UINT8> mIm(pyobj);
-	  TEST_ASSERT(mIm.isAllocated());
-	  
-	  Py_Finalize();
-	    
-	}
-    };
-#endif // WRAP_PYTHON
-
 int main(int argc, char *argv[])
 {
       TestSuite ts;
       
-#ifdef WRAP_PYTHON
-      ADD_TEST(ts, Test_Python_Import);
-#endif // WRAP_PYTHON
       
       return ts.run();
   
