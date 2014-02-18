066eb3f6c747b6c23488a71d8b4ce2f8c453a3f3
Théodore Chabardès <theodore@theodore-Precision-WorkStation-390.(none)>
2014-02-18 16:33:33 +0100

    Added a test case for the new distance function.

---
+++ Morpho/test/bench_distance.cpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2011, Matthieu FAESSEL and ARMINES
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Matthieu FAESSEL, or ARMINES nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+
+#include "DCore.h"
+#include "DMorpho.h"
+
+using namespace smil;
+
+int main(int argc, char *argv[])
+{
+    Image_UINT8 im1(500, 500);
+    Image_UINT16 im2(im1);
+    Image_UINT16 im3(im1);
+    
+    fill(im1, UINT8(255));
+    drawRectangle(im1, 10,10,400,400, UINT8(0), true);
+    im3.clone (im1);
+    dist (im3, im3, CrossSE());
+
+    UINT BENCH_NRUNS = 1E2;
+    BENCH_IMG(dist_v2, im1, im2, CrossSE());
+    TEST_ASSERT (im2==im3);
+
+    if (retVal != RES_OK)
+       im2.printSelf (1); 
+}
+
