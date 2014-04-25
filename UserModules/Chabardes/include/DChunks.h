6e824ea52d79a21c7910c84dee48e59d4c607bf2
Chabardes <theodore@chabardes.net>
2014-04-25 18:05:16 +0200

    Create a chunk class and a continuous space of memory to be able to send everything at once with mpi and use specialized function like MPI_Scatter & MPI_Gather.

--- UserModules/Chabardes/include/DChunks.h
+++ UserModules/Chabardes/include/DChunks.h
@@ -6,16 +6,103 @@
 #include "mpi.h"
 
 namespace smil {
+
     template <class T>
-    struct Chunk {
-        int size[3];
-        int offset[3];
-        T* data;
+    class Chunk {
+        public:
+            Chunk (): rawData(NULL)  { }
+            ~Chunk () {
+                if (rawData != NULL) 
+                    ::operator delete(rawData);
+            }
+            RES_T allocate (const size_t &s) {
+                rawData = ::operator new (sizeof(int)*6+sizeof(T)*s);
+                if (rawData == NULL) { return RES_ERR_BAD_ALLOCATION; } 
+                size = (int*)rawData;
+                offset = (int*)rawData + 3;
+                data = (T*)((int*)rawData + 6);                
+            }
+            RES_T createFromArray (
+                    const T* dataIn,
+                    const int &sd_x, 
+                    const int &sd_y,
+                    const int &sc_x,
+                    const int &sc_y,
+                    const int &sc_z,
+                    const int &o_x,
+                    const int &o_y,
+                    const int &o_z) {
+                size[0] = sc_x; size[1] = sc_y; size[2] = sc_z;
+                offset[0] = o_x; offset[1] = o_y; offset[2] = o_z; 
+
+                for (int k=0; k<sc_z; ++k) {
+                    for (int j=0; j<sc_y; ++j) {
+                        if (memcpy (data + j * sc_x + k * sc_x * sc_y, 
+                                    dataIn + o_x + (o_y+j) * sd_x + (o_z+k) * sd_x * sd_y,
+                                    sc_x*sizeof(T)) == NULL){
+                            return RES_ERR_IO;
+                        }
+                    }
+                }
+                return RES_OK;
+                   
+            }
+            RES_T storeToArray (const int &sd_x, const int &sd_y, T* dataOut) {
+                for (int k=0; k<size[2]; ++k) {
+                    for (int j=0; j<size[1]; ++j) {
+                        if (memcpy (dataOut + offset[0] + (offset[1]+j) * sd_x + (offset[2]+k) * sd_x * sd_y,
+                                    data + j * size[0] + k * size[0] * size[1], 
+                                    size[0]*sizeof(T)) == NULL){
+                            return RES_ERR_IO;
+                        }
+                    }
+                }
+                return RES_OK;
+            } 
+            RES_T storeToImage (Image<T> &imOut) {
+//              ASSERT (all the good stuff).
+
+                size_t s[3];
+                imOut.getSize(s);
+
+                if (storeToArray (s[0], s[1], imOut.getPixels()) != RES_OK) return RES_ERR;
+                return RES_OK;
+            }
+            int getSize (const unsigned int &dimension) {
+                ASSERT (dimension < 3) ;
+                return size[dimension] ;
+            }
+            int getOffset (const unsigned int &dimension) {
+                ASSERT (dimension < 3) ;
+                return offset[dimension] ;
+
+            }
+            T* getData () {
+                return data;
+            }
+            void print (bool print_data = false) {
+                ASSERT (rawData!=NULL) ;
+                cout << "#### size : [" << size[0] << ", " << size[1] << ", " << size[2] << "], offset : [" << offset[0] << ", " << offset[1] << ", " << offset[2] << "] ####" << endl;
+                if (!print_data) return;
+                for (int i=0; i<size[2]; ++i) {
+                    for (int j=0; j<size[1]; ++j) {
+                        for (int k=0; k<size[0]; ++k)
+                            cout << (int) data[k+j*size[0]+i*size[0]*size[1]] << ' ';
+                        cout << endl;
+                    }
+                    cout << endl;
+                }
+                cout << endl; 
+            }
+        private:
+            int* size;
+            int* offset;
+            T* data;
+            void* rawData;
     };
 
     struct Chunks_Header {
         int datum_size;
-        int mpi_type;
         int nbr_chunks;
         int size_chunks;
         int nbr_borders;
@@ -23,13 +110,8 @@ namespace smil {
         int nbr_intersects;
         int size_intersects;
         int ncpd[3]; // number of chunks per dimensions.
-    };
-
-    template <class T>
-    struct Chunks_Data {
-        Chunk<UINT8>* ca; // chunks
-        Chunk<UINT8>* ba; // borders
-        Chunk<UINT8>* ia; // intersections  
+        int mpi_type;
+        MPI_Datatype mpi_chunks_types[3];
     };
 
     int smilToMPIType (const char* type_datum) {
@@ -46,41 +128,6 @@ namespace smil {
         } else {
                 return MPI_UNSIGNED;
         }
-    }
-
-    template <class T>
-    void printChunk (Chunk<T> c, bool print_data=false) {
-        cout << "#### size : [" << c.size[0] << ", " << c.size[1] << ", " << c.size[2] << "], offset : [" << c.offset[0] << ", " << c.offset[1] << ", " << c.offset[2] << "] ####" << endl;
-        if (!print_data) return;
-        for (int i=0; i<c.size[2]; ++i) {
-            for (int j=0; j<c.size[1]; ++j) {
-                for (int k=0; k<c.size[0]; ++k)
-                    cout << (int) c.data[k+j*c.size[0]+i*c.size[0]*c.size[1]] << ' ';
-                cout << endl;
-            }
-            cout << endl;
-        }
-        cout << endl;
-    }
-
-    template <class T>
-    RES_T copyChunkFromArray (const T* dataIn, const int o_x, const int o_y, const int o_z, const int s_x, const int s_y, const int s_z, const int sd_x, const int sd_y, Chunk<T>& c) {
-//        cout << "offset : [" << o_x << ", " << o_y << ", " << o_z << "], size : [" << s_x << ", " << s_y << ", " << s_z << "]." << endl;
-
-        c.size[0] = s_x; c.size[1] = s_y; c.size[2] = s_z;
-        c.offset[0] = o_x; c.offset[1] = o_y; c.offset[2] = o_z; 
-        c.data = new T[s_x*s_y*s_z];
-
-        for (int k=0; k<s_z; ++k) {
-            for (int j=0; j<s_y; ++j) {
-                if (memcpy (c.data + j * s_x + k * s_x * s_y, 
-                            dataIn + o_x + (o_y+j) * sd_x + (o_z+k) * sd_x * sd_y,
-                            s_x*sizeof(T)) == NULL){
-                    return RES_ERR_IO;
-                }
-            }
-        }
-        return RES_OK;
     }
 
     template <class T>
@@ -103,28 +150,32 @@ namespace smil {
         }
   
         ch.nbr_borders = ch.ncpd[0]*ch.ncpd[1] + ch.ncpd[0]*ch.ncpd[2] + ch.ncpd[1]*ch.ncpd[2] -ch.ncpd[0] -ch.ncpd[1] -ch.ncpd[2] +1;
-        ch.size_borders =  (s[0 - s[0]/ch.ncpd[0]] * ch.ncpd[0]-1) *
-                            (s[1 - s[1]/ch.ncpd[1]] * ch.ncpd[1]-1) *
-                            (s[2 - s[2]/ch.ncpd[2]] * ch.ncpd[2]-1);
+        ch.size_borders =  (s[0] - s[0]/ch.ncpd[0] * (ch.ncpd[0]-1)) *
+                            (s[1] - s[1]/ch.ncpd[1] * (ch.ncpd[1]-1)) *
+                            (s[2] - s[2]/ch.ncpd[2] * (ch.ncpd[2]-1));
         ch.nbr_chunks = ch.ncpd[0]*ch.ncpd[1]*ch.ncpd[2] - ch.nbr_borders;
         ch.size_chunks = s[0]/ch.ncpd[0] * s[1]/ch.ncpd[1] * s[2]/ch.ncpd[2];
         ch.nbr_intersects = ch.ncpd[0] + ch.ncpd[1] + ch.ncpd[2] -3;
         int max_dimension = MAX(s[1],s[2]);
         max_dimension = MAX(max_dimension,s[0]);
         ch.size_intersects = intersect_width * max_dimension*max_dimension; 
-
-//        cout << "number of chunks : " << ch.nbr_chunks+ch.nbr_borders << " [" << ch.ncpd[0] << ", " << ch.ncpd[1] << ", " << ch.ncpd[2] << "], number of intersects : " << ch.nbr_intersects << endl; 
     }
 
     template <class T>
-    RES_T createChunks (const Image<T> &imIn, const Chunks_Header &ch, Chunks_Data<T> &cd) {
+    RES_T createChunks (const Image<T> &imIn, const Chunks_Header &ch, Chunk<T> *ca) {
         RES_T err;
+
+        for (int i=0; i<ch.nbr_chunks; ++i) {
+            ca[i].allocate (ch.size_chunks);
+        }
+        for (int i=0;i<ch.nbr_borders;++i) {
+            ca[ch.nbr_chunks+i].allocate (ch.size_borders);
+        }
+
         size_t s[3]; 
         imIn.getSize(s);
 
-        cd.ca = new Chunk<T>[ch.nbr_chunks];
-        cd.ba = new Chunk<T>[ch.nbr_borders];
-        Chunk<T>* target;
+        int target; 
         int v=0, w=0;
 
         int s_x, s_y, s_z;
@@ -134,20 +185,19 @@ namespace smil {
                     s_x = (i == ch.ncpd[0]-1 ) ? s[0] - (s[0]/ch.ncpd[0]) * i : s[0]/ch.ncpd[0] ;
                     s_y = (j == ch.ncpd[1]-1 ) ? s[1] - (s[1]/ch.ncpd[1]) * j : s[1]/ch.ncpd[1] ;
                     s_z = (k == ch.ncpd[2]-1 ) ? s[2] - (s[2]/ch.ncpd[2]) * k : s[2]/ch.ncpd[2] ;
-                    if (i == ch.ncpd[0]-1 || j == ch.ncpd[1]-1 || k == ch.ncpd[2]-1) {target = cd.ba + (v++);}
-                    else {target = cd.ca + (w++);}
+                    if (i == ch.ncpd[0]-1 || j == ch.ncpd[1]-1 || k == ch.ncpd[2]-1) {target = ch.nbr_chunks + (v++);}
+                    else {target = w++;}
 
-                    err = copyChunkFromArray ((T*)imIn.getPixels(),
+                    if (ca[target].createFromArray (
+                                        (T*)imIn.getPixels(),
+                                        s[0], s[1], 
                                         i*s[0]/ch.ncpd[0], 
                                         j*s[1]/ch.ncpd[1], 
                                         k*s[2]/ch.ncpd[2],
                                         s_x,
                                         s_y,
-                                        s_z,
-                                        s[0], s[1], 
-                                        *target
-                                       );
-                    if (err != RES_OK)
+                                        s_z
+                                       ) != RES_OK)
                         return RES_ERR_BAD_ALLOCATION;
                 }
             }
@@ -156,54 +206,52 @@ namespace smil {
     }
 
     template <class T>
-    RES_T createIntersects (const Image<T> &imIn, const int& intersect_width, const Chunks_Header &ch, Chunks_Data<T> &cd) {
+    RES_T createIntersects (const Image<T> &imIn, const int& intersect_width, const Chunks_Header &ch, Chunk<T>* ca) {
         RES_T err;
+
+        for (int i=0; i<ch.nbr_intersects; ++i) {
+            ca[ch.nbr_chunks+ch.nbr_borders+i].allocate (ch.size_intersects) ;
+        }
+
         size_t s[3];
         imIn.getSize(s);
 
-        cd.ia = new Chunk<T>[ch.nbr_intersects];
         int j=0;
 
         for (int i=1; i<ch.ncpd[0]; ++i, ++j) {
-            err = copyChunkFromArray (
+            ca[ch.nbr_chunks+ch.nbr_borders+j].createFromArray (
                                      (T*)imIn.getPixels(),
+                                      s[0], s[1],
                                       i*s[0]/ch.ncpd[0]-intersect_width/2,
                                       0,
                                       0,
                                       intersect_width,
                                       s[1],
-                                      s[2],
-                                      s[0],
-                                      s[1],
-                                      cd.ia[j]
+                                      s[2]
                                      );
         }
         for (int i=1; i<ch.ncpd[1]; ++i, ++j) {
-            err = copyChunkFromArray (
+            ca[ch.nbr_chunks+ch.nbr_borders+j].createFromArray (
                                      (T*)imIn.getPixels(), 
+                                      s[0], s[1],
                                       0,
                                       i*s[1]/ch.ncpd[1]-intersect_width/2,
                                       0,
                                       s[0],
                                       intersect_width,
-                                      s[2],
-                                      s[0],
-                                      s[1],
-                                      cd.ia[j]
+                                      s[2]
                                      );
         }
         for (int i=1; i<ch.ncpd[2]; ++i, ++j) {
-            err = copyChunkFromArray (
+            ca[ch.nbr_chunks+ch.nbr_borders+j].createFromArray (
                                       (T*)imIn.getPixels(), 
+                                      s[0], s[1],
                                       0,
                                       0,
                                       i*s[2]/ch.ncpd[2]-intersect_width/2,
                                       s[0],
                                       s[1],
-                                      intersect_width,
-                                      s[0],
-                                      s[1],
-                                      cd.ia[j]
+                                      intersect_width
                                      );
         }
         return RES_OK;
@@ -211,55 +259,35 @@ namespace smil {
 
 // TODO: check why imIn has to be not const to use getTypeAsString () .
     template <class T>
-    RES_T generateChunks (Image<T> &imIn, const int size_simultaneously_calculated, const int size_chunk_max, const int nbr_cores, const StrElt &se, const int intersect_width, Chunks_Header &ch, Chunks_Data<T> &cd ) {
+    RES_T generateChunks (Image<T> &imIn, const int size_simultaneously_calculated, const int size_chunk_max, const int nbr_cores, const StrElt &se, const int intersect_width, Chunks_Header &ch, Chunk<T>* &ca ) {
         getChunksHeader (imIn, size_simultaneously_calculated, size_chunk_max, nbr_cores, se, intersect_width, ch);
-        createChunks (imIn, ch, cd);
-        createIntersects (imIn, intersect_width, ch, cd);
+        ca = new Chunk<T>[ch.nbr_chunks+ch.nbr_borders+ch.nbr_intersects];
+        createChunks (imIn, ch, ca);
+        createIntersects (imIn, intersect_width, ch, ca);
     }
 
-
-
-    int registerChunks (const Chunks_Header &ch, MPI_Datatype &mpi_chunk_type, MPI_Datatype &mpi_border_type, MPI_Datatype &mpi_intersect_type) {
+    void registerChunks (Chunks_Header &ch) { 
 //    Broadcast the infos on the chunks.
         MPI_Bcast ((void*)&ch, 11, MPI_INTEGER, 0, MPI_COMM_WORLD);
-
-        MPI_Datatype types[2] = {MPI_INTEGER, ch.mpi_type};
-        int blocks_lengths[2] = {6,ch.nbr_chunks};
-        MPI_Aint offsets[2] = {0, 6*sizeof(int)} ;
-
-//    CHUNKS
-        MPI_Type_create_struct (2, blocks_lengths, offsets, types, &mpi_chunk_type) ;
-        MPI_Type_commit (&mpi_chunk_type);
+        MPI_Datatype old_types[2] = {MPI_INTEGER, ch.mpi_type};
+        MPI_Aint steps[2] = {0,6*sizeof(int)};
+        int blocks_sizes [2]; blocks_sizes[0] = 6; 
+ 
+//    CHUNKS 
+        blocks_sizes[1] = ch.size_chunks;
+        MPI_Type_struct (2, blocks_sizes, steps, old_types, ch.mpi_chunks_types);
 //    BORDERS
-        blocks_lengths[1] = ch.nbr_borders;
-        MPI_Type_create_struct (2, blocks_lengths, offsets, types, &mpi_border_type) ;
-        MPI_Type_commit (&mpi_chunk_type);
+        blocks_sizes[1] = ch.size_borders;
+        MPI_Type_struct (2, blocks_sizes, steps, old_types, ch.mpi_chunks_types+1);
 //    INTERSECTS
-        blocks_lengths[1] = ch.nbr_intersects;
-        MPI_Type_create_struct (2, blocks_lengths, offsets, types, &mpi_intersect_type) ;
-        MPI_Type_commit (&mpi_chunk_type);
+        blocks_sizes[2] = ch.size_intersects;
+        MPI_Type_struct (2, blocks_sizes, steps, old_types, ch.mpi_chunks_types+2);
     }
 
-    RES_T unregisterChunks (const Chunks_Header &ch, MPI_Datatype &mpi_chunk_type, MPI_Datatype &mpi_border_type, MPI_Datatype &mpi_intersect_type) {
-        MPI_Type_free (&mpi_chunk_type);
-        MPI_Type_free (&mpi_border_type);
-        MPI_Type_free (&mpi_intersect_type);
-    }
-
-    template <class T>
-    RES_T deleteChunks (const Chunks_Header &ch, Chunks_Data<T> &cd) {
-        for (int i=0; i<ch.nbr_chunks; ++i) {
-            delete cd.ca[i].data;
+    void unregisterChunks (Chunks_Header &ch) {
+        for (int i=0; i<3; ++i) {
+            MPI_Type_free (ch.mpi_chunks_types+i) ;
         }
-        delete cd.ca;
-        for (int i=0; i<ch.nbr_borders; ++i) {
-            delete cd.ba[i].data;
-        }
-        delete cd.ba;
-        for (int i=0; i<ch.nbr_intersects; ++i) {
-            delete cd.ia[i].data;
-        }
-        delete cd.ia;
     }
 }
 
--- UserModules/Chabardes/test/test_mpi_morpho_base.cpp
+++ UserModules/Chabardes/test/test_mpi_morpho_base.cpp
@@ -1,62 +1,79 @@
 #include "DChabardes.h"
 #include "DCpuID.h"
+#include <unistd.h>
 
 using namespace smil;
 
-int master_proc (int argc, char* argv[], int nbr_procs, int rank) {
+RES_T processChunk (UINT8 value, Chunk<UINT8> &c) {
+    int size = c.getSize(0)*c.getSize(1)*c.getSize(2);
+    UINT8* data = c.getData () ;
+    for (int i=0; i<size; ++i) {
+        data[i] = value; 
+    }
+    return RES_OK;
+} 
+
+int master_proc (int argc, char* argv[], const int nbr_procs, const int rank) {
     int err_code;
-    MPI_Datatype mpi_chunk_type, mpi_border_type, mpi_intersect_type;
+    int nbr_chunks_processed = 0;
 
     CpuID cpu;
-    Cache_Descriptors L1 = cpu.getCaches().front();
-    Cache_Descriptors LL = cpu.getCaches().back();
+    Cache_Descriptors L1;// = cpu.getCaches().front();
+    L1.size = 6;
+    Cache_Descriptors LL;// = cpu.getCaches().back();
+    LL.size = 541;
     Image<UINT8> im;
     Cross3DSE se;
-    Chunks_Data<UINT8> cd;
+    Chunk<UINT8>* ca;
     Chunks_Header ch; 
 
-    im = Image<UINT8> (300,300,300);
+    im = Image<UINT8> (30,30,30);
     fill (im, UINT8(0));
-    generateChunks (im, L1.size, LL.size, nbr_procs, se, 4, ch, cd);
-
-/*        
-    cout << "CHUNKS" << endl;
-    for (int i=0; i<cd.nbr_chunks; ++i) {
-        printChunk (cd.ca[i]);
-    }
-    cout << "BORDERS" << endl;
-    for (int i=0; i<cd.nbr_borders; ++i) {
-        printChunk (cd.ba[i]);
-    }
-    cout << "INTERSECTS" << endl;
-    for (int i=0; i<cd.nbr_intersects; ++i) {
-        printChunk (cd.ia[i]);
-    }
-*/
-
-    registerChunks (ch, mpi_chunk_type, mpi_border_type, mpi_intersect_type);
+    generateChunks (im, L1.size, LL.size, nbr_procs, se, 3, ch, ca);
+    registerChunks (ch) ;
 
     // Processing.
-
-//    write (im, );
-    deleteChunks (ch, cd);
-
-    unregisterChunks (ch, mpi_chunk_type, mpi_border_type, mpi_intersect_type);
-
-
+    // Scatter
+    /*
+    MPI_Status status;
+    Chunk<UINT8> c;
+    int max_dimension = MAX (ch.size_borders, ch.size_intersects);
+    c.data = new UINT8[max_dimension];
+    while (nbr_chunks_processed < ch.nbr_chunks+ch.nbr_borders+ch.nbr_intersects) {
+        recvChunk (ch, MPI_ANY_SOURCE, MPI_ANY_TAG, c, &status) ;
+        //cout << "[0] receive chunk from " << status.MPI_SOURCE << endl;
+        storeChunk (ch, c, im);
+        if (nbr_chunks_processed < ch.nbr_chunks+ch.nbr_borders+ch.nbr_intersects) {
+            sendChunk (ch, ca[nbr_chunks_processed++], status.MPI_SOURCE, status.MPI_TAG) ;
+            //cout << "[0] send chunk to " << status.MPI_SOURCE << endl;
+        }
+    }
+    */
+    im.save ( "/tmp/test.png" );
+    unregisterChunks (ch) ;
+    delete (ca);
 }
 
 int slave_proc (int nbr_procs, int rank) {
     int err_code;
-    MPI_Datatype mpi_chunk_type, mpi_border_type, mpi_intersect_type;
 
     Chunks_Header ch; 
-
-    registerChunks (ch, mpi_chunk_type, mpi_border_type, mpi_intersect_type);
+    registerChunks (ch) ;
 
     // Processing.
-
-    unregisterChunks (ch, mpi_chunk_type, mpi_border_type, mpi_intersect_type);
+    Chunk<UINT8> c;
+    int max_dimension = MAX (ch.size_borders, ch.size_intersects);
+    c.allocate (max_dimension) ; 
+    /* 
+    while (1) {
+        recvChunk (ch, 0, rank*10, c, MPI_STATUS_IGNORE);
+        sleep (1);
+        //cout << "[" << rank << "] receive chunk from " << 0 << endl;
+        processChunk (rank*10, c);
+        sendChunk (ch, c, 0, rank*10);
+        //cout << "[" << rank << "] send chunk to " << 0 << endl;
+    }
+    */
 }
 
 int main(int argc, char* argv[]){
@@ -76,6 +93,7 @@ int main(int argc, char* argv[]){
     } else {
         err_code = slave_proc (nbr_procs, rank);
     }
+    cout << rank << " : exited" << endl;
 
     err_code = MPI_Finalize ();
     return err_code;
