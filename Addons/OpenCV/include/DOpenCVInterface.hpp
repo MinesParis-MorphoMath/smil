6de1b8c39bfe8421dcdb83ccf63a7dc785b311f6
Matthieu Faessel <faessel@cmm.ensmp.fr>
2014-07-17 12:13:57 +0200

    Add Smil to OpenCV Mat function.

--- Addons/OpenCV/include/DOpenCVInterface.hpp
+++ Addons/OpenCV/include/DOpenCVInterface.hpp
@@ -105,6 +105,29 @@ namespace smil
     IplImage *toIplImage(Image<T> &im)
     {
     }
+    
+    template <class T>
+    int getCvType()
+    {
+      return -1;
+    }
+    template<> int getCvType<UINT8>() { return CV_8UC1; }
+    template<> int getCvType<UINT16>() { return CV_16UC1; }
+    
+    /**
+     * Create a OpneCV Mat from a Smil image (the data is copied)
+     */    
+    template <class T>
+    cv::Mat toMatImage(Image<T> &im)
+    {
+	int cvType = getCvType<T>();
+	
+	ASSERT(cvType!=-1, "Data type conversion not implemented", cv::Mat())
+	
+	return cv::Mat(im.getHeight(), im.getWidth(), cvType, im.getPixels());
+    }
+
+
 
    /*@}*/
     
