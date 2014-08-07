80489fb746a2828fed3b97c94e4c2d79a08afa35
Matthieu Faessel <faessel@cmm.ensmp.fr>
2014-08-07 15:06:24 +0200

    Add areaOpen (gray level) function (maxtree version)

--- Morpho/test/test_maxTree.cpp
+++ Morpho/test/test_maxTree.cpp
@@ -186,12 +186,138 @@ class Test_DeltaUO : public TestCase
   }
 };
 
+class Test_UO_MSER : public TestCase
+{
+  virtual void run()
+  {
+      typedef UINT8 dataType1;
+      typedef UINT16 dataType2;
+      
+      typedef Image<dataType1> imType1;
+      typedef Image<dataType2> imType2;
+      
+      imType1 im1(8,8);
+      imType1 im2(im1);
+      imType1 im3(im1);
+      
+      imType2 imLbl(im1);
+      
+      dataType1 vec1[] = 
+      {
+	0,   0,   0,   0,   0,   0,   0,   0,
+	0,  10,  10,  10,  10,  10,  10,   0,
+	0,  10,  40,  40,  40,  40,  10,   0,
+	0,  10,  40,  50,  50,  50,  10,   0,
+	0,  10,  40,  50,  60,  50,  10,   0,
+	0,  10,  40,  50,  50,  50,  10,   0,
+	0,  10,  10,  10,  10,  10,  10,   0,
+	0,   0,   0,   0,   0,   0,   0,   0,
+      };
+      
+      im1 << vec1;
+      
+      ultimateOpenMSER(im1, im2, imLbl, 7, 1);
+      
+      dataType1 vecTrans[] =
+      {
+	0,   0,   0,   0,   0,   0,   0,   0,
+	0,   5,   5,   5,   5,   5,   5,   0,
+	0,   5,  13,  13,  13,  13,   5,   0,
+	0,   5,  13,  13,  13,  13,   5,   0,
+	0,   5,  13,  13,  13,  13,   5,   0,
+	0,   5,  13,  13,  13,  13,   5,   0,
+	0,   5,   5,   5,   5,   5,   5,   0,
+	0,   0,   0,   0,   0,   0,   0,   0,
+      };
+      
+      dataType2 vecIndic[] =
+      {
+	0,     0,     0,     0,     0,     0,     0,     0,
+	0,     7,     7,     7,     7,     7,     7,     0,
+	0,     7,     5,     5,     5,     5,     7,     0,
+	0,     7,     5,     5,     5,     5,     7,     0,
+	0,     7,     5,     5,     5,     5,     7,     0,
+	0,     7,     5,     5,     5,     5,     7,     0,
+	0,     7,     7,     7,     7,     7,     7,     0,
+	0,     0,     0,     0,     0,     0,     0,     0,
+      };
+      
+      imType1 imTrans(im1);
+      imType2 imIndic(imLbl);
+      
+      imTrans << vecTrans;
+      TEST_ASSERT(im2==imTrans);
+      
+      imIndic << vecIndic;
+      TEST_ASSERT(imLbl==imIndic);
+      
+      if (retVal!=RES_OK)
+      {
+	im2.printSelf(1);
+	imLbl.printSelf(1);
+      }
+  }
+};
+
+class Test_AttributeOpening : public TestCase
+{
+  virtual void run()
+  {
+      typedef UINT8 dataType;
+      typedef Image<dataType> imType;
+      
+      imType im1(8,8);
+      imType im2(im1);
+      
+      dataType vec1[] = 
+      {
+	0,   0,   0,   0,   0,   0,   0,   0,
+	0,  10,  10,  10,  10,  10,  10,   0,
+	0,  10,  40,  40,  40,  40,  10,   0,
+	0,  10,  40,  50,  50,  50,  10,   0,
+	0,  10,  40,  50,  60,  50,  10,   0,
+	0,  10,  40,  50,  50,  50,  10,   0,
+	0,  10,  10,  10,  10,  10,  10,   0,
+	0,   0,   0,   0,   0,   0,   0,   0,
+      };
+      
+      im1 << vec1;
+      
+      areaOpen(im1, 10, im2);
+      
+      dataType vec2[] =
+      {
+	0,   0,   0,   0,   0,   0,   0,   0,
+	0,  10,  10,  10,  10,  10,  10,   0,
+	0,  10,  40,  40,  40,  40,  10,   0,
+	0,  10,  40,  40,  40,  40,  10,   0,
+	0,  10,  40,  40,  40,  40,  10,   0,
+	0,  10,  40,  40,  40,  40,  10,   0,
+	0,  10,  10,  10,  10,  10,  10,   0,
+	0,   0,   0,   0,   0,   0,   0,   0,
+      };
+      
+      imType imTruth(im1);
+      
+      imTruth << vec2;
+      TEST_ASSERT(im2==imTruth);
+      
+      if (retVal!=RES_OK)
+      {
+	im2.printSelf(1);
+	imTruth.printSelf(1);
+      }
+  }
+};
+
 
 int main(int argc, char *argv[])
 {
       TestSuite ts;
       ADD_TEST(ts, Test_MaxTree);
       ADD_TEST(ts, Test_DeltaUO);
+      ADD_TEST(ts, Test_UO_MSER);
+      ADD_TEST(ts, Test_AttributeOpening);
       return ts.run();
 }
 
