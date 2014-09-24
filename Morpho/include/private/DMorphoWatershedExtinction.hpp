6c5063065271c8048edd3a806afbd2dd9873b690
Marcotegui <beatriz.marcotegui@mines-paristech.fr>
2014-09-24 12:28:17 +0200

    watershedExtinction: versions for dynamic and area extinction values added
    (only for image-based version, not graph).
    
    By default, volume extinction value is used.

--- Morpho/include/private/DMorphoWatershedExtinction.hpp
+++ Morpho/include/private/DMorphoWatershedExtinction.hpp
@@ -114,7 +114,7 @@ namespace smil
     }
     template < class T, class labelT,
 	class HQ_Type >
-	RES_T processWatershedHierarchicalQueueExtinction (const Image < T >
+	RES_T processVolWatershedHierarchicalQueueExtinction (const Image < T >
 							   &imIn,
 							   Image < labelT >
 							   &imLbl,
@@ -245,6 +245,274 @@ namespace smil
 
 	return RES_OK;
     }
+    template < class T, class labelT,
+	class HQ_Type >
+	RES_T processAreaWatershedHierarchicalQueueExtinction (const Image < T >
+							   &imIn,
+							   Image < labelT >
+							   &imLbl,
+							   HQ_Type & hq,
+							   const StrElt & se,
+							   basin < labelT >
+							   *e,
+							   UINT nbr_label) {
+	typename ImDtTypes < T >::lineType inPixels = imIn.getPixels ();
+	typename ImDtTypes < labelT >::lineType lblPixels =
+	    imLbl.getPixels ();
+	vector < int >dOffsets;
+
+	vector < IntPoint >::const_iterator it_start = se.points.begin ();
+	vector < IntPoint >::const_iterator it_end = se.points.end ();
+	vector < IntPoint >::const_iterator it;
+	vector < UINT > tmpOffsets;
+	size_t s[3];
+
+	imIn.getSize (s);
+
+	T level_before = 0;
+	size_t curOffset;
+
+	// set an offset distance for each se point
+	for (it = it_start; it != it_end; it++) {
+	    dOffsets.push_back (it->x + it->y * s[0] + it->z * s[0] * s[1]);
+	}
+	vector < int >::iterator it_off_start = dOffsets.begin ();
+	vector < int >::iterator it_off;
+
+	while (!hq.isEmpty ()) {
+	    curOffset = hq.pop ();
+
+	    // Rising the elevation of the basins
+	    if (inPixels[curOffset] > level_before) {
+		for (labelT i = 1; i < nbr_label + 1; ++i) {
+		    if (e[i].val_of_min < inPixels[curOffset]) {
+			e[i].vol +=
+			    e[i].area * (inPixels[curOffset] -
+					 e[i].val_of_min);
+			e[i].val_of_min = inPixels[curOffset];
+		    }
+		}
+		level_before = inPixels[curOffset];
+	    }
+
+	    size_t x0, y0, z0;
+
+	    imIn.getCoordsFromOffset (curOffset, x0, y0, z0);
+	    bool oddLine = se.odd && ((y0) % 2);
+	    int x, y, z;
+	    size_t nbOffset;
+	    labelT l1 = lblPixels[curOffset], l2;
+
+	    for (it = it_start, it_off = it_off_start; it != it_end;
+		 it++, it_off++)
+		if (it->x != 0 || it->y != 0 || it->z != 0)	// useless if x=0 & y=0 & z=0
+		{
+		    x = x0 + it->x;
+		    y = y0 + it->y;
+		    z = z0 + it->z;
+		    if (oddLine)
+			x += (((y + 1) % 2) != 0);
+		    if (x >= 0 && x < (int) s[0] && y >= 0 && y < (int) s[1]
+			&& z >= 0 && z < (int) s[2]) {
+			nbOffset = curOffset + *it_off;
+			if (oddLine)
+			    nbOffset += (((y + 1) % 2) != 0);
+			l2 = lblPixels[nbOffset];
+			if (l2 > 0 && l2 != nbr_label + 1) {
+			    while (l2 != e[l2].equivalent) {
+				l2 = e[l2].equivalent;
+			    }
+
+			    if (l1 == 0 || l1 == nbr_label + 1) {
+				l1 = l2;
+				lblPixels[curOffset] = l1;
+				e[l1].area++;
+			    }
+			    else if (l1 != l2) {
+
+				while (l1 != e[l1].equivalent) {
+				    l1 = e[l1].equivalent;
+				}
+				if (l1 != l2) {
+				    if (e[l1].area > e[l2].area
+					|| (e[l1].area == e[l2].area
+					    && e[l1].val_of_min <
+					    e[l2].val_of_min)) {
+					/* cur basin absorbs nb basin */
+					e[l2].vol_ex_val = e[l2].area;
+					e[l1].vol += e[l2].vol;
+					e[l1].area += e[l2].area;
+					e[l2].equivalent = l1;
+				    }
+				    else {
+					/* nb basin absorbs cur basin */
+					e[l1].vol_ex_val = e[l1].area;
+					e[l2].vol += e[l1].vol;
+					e[l2].area += e[l1].area;
+					e[l1].equivalent = l2;
+					l1 = l2;
+				    }
+				}
+			    }
+			}
+			else if (l2 == 0) {	// Add it to the tmp offsets queue
+			    tmpOffsets.push_back (nbOffset);
+			    lblPixels[nbOffset] = nbr_label + 1;
+			}
+		    }
+		}
+	    if (!tmpOffsets.empty ()) {
+		typename vector < UINT >::iterator t_it = tmpOffsets.begin ();
+
+		while (t_it != tmpOffsets.end ()) {
+		    hq.push (inPixels[*t_it], *t_it);
+		    t_it++;
+		}
+	    }
+	    tmpOffsets.clear ();
+	}
+
+	// Update Last level of flooding.
+	e[lblPixels[curOffset]].vol += e[lblPixels[curOffset]].area;
+	e[lblPixels[curOffset]].vol_ex_val += e[lblPixels[curOffset]].area;
+
+	return RES_OK;
+    }
+
+
+    template < class T, class labelT,
+	class HQ_Type >
+	RES_T processDynWatershedHierarchicalQueueExtinction (const Image < T >
+							   &imIn,
+							   Image < labelT >
+							   &imLbl,
+							   HQ_Type & hq,
+							   const StrElt & se,
+							   basin < labelT >
+							   *e,
+							   UINT nbr_label) {
+	typename ImDtTypes < T >::lineType inPixels = imIn.getPixels ();
+	typename ImDtTypes < labelT >::lineType lblPixels =
+	    imLbl.getPixels ();
+	vector < int >dOffsets;
+
+	vector < IntPoint >::const_iterator it_start = se.points.begin ();
+	vector < IntPoint >::const_iterator it_end = se.points.end ();
+	vector < IntPoint >::const_iterator it;
+	vector < UINT > tmpOffsets;
+	size_t s[3];
+
+	imIn.getSize (s);
+
+	T level_before = 0, current_level;
+	size_t curOffset;
+
+	// set an offset distance for each se point
+	for (it = it_start; it != it_end; it++) {
+	    dOffsets.push_back (it->x + it->y * s[0] + it->z * s[0] * s[1]);
+	}
+	vector < int >::iterator it_off_start = dOffsets.begin ();
+	vector < int >::iterator it_off;
+
+	while (!hq.isEmpty ()) {
+	    curOffset = hq.pop ();
+
+	    // Rising the elevation of the basins
+	    if (inPixels[curOffset] > level_before) {
+		for (labelT i = 1; i < nbr_label + 1; ++i) {
+		    if (e[i].val_of_min < inPixels[curOffset]) {
+			e[i].vol +=
+			    e[i].area * (inPixels[curOffset] -
+					 e[i].val_of_min);
+			e[i].val_of_min = inPixels[curOffset];
+		    }
+		}
+		level_before = inPixels[curOffset];
+	    }
+
+	    size_t x0, y0, z0;
+
+	    imIn.getCoordsFromOffset (curOffset, x0, y0, z0);
+	    bool oddLine = se.odd && ((y0) % 2);
+	    int x, y, z;
+	    size_t nbOffset;
+	    labelT l1 = lblPixels[curOffset], l2;
+	    current_level = inPixels[curOffset];
+	    for (it = it_start, it_off = it_off_start; it != it_end;
+		 it++, it_off++)
+		if (it->x != 0 || it->y != 0 || it->z != 0)	// useless if x=0 & y=0 & z=0
+		{
+		    x = x0 + it->x;
+		    y = y0 + it->y;
+		    z = z0 + it->z;
+		    if (oddLine)
+			x += (((y + 1) % 2) != 0);
+		    if (x >= 0 && x < (int) s[0] && y >= 0 && y < (int) s[1]
+			&& z >= 0 && z < (int) s[2]) {
+			nbOffset = curOffset + *it_off;
+			if (oddLine)
+			    nbOffset += (((y + 1) % 2) != 0);
+			l2 = lblPixels[nbOffset];
+			if (l2 > 0 && l2 != nbr_label + 1) {
+			    while (l2 != e[l2].equivalent) {
+				l2 = e[l2].equivalent;
+			    }
+
+			    if (l1 == 0 || l1 == nbr_label + 1) {
+				l1 = l2;
+				lblPixels[curOffset] = l1;
+				e[l1].area++;
+			    }
+			    else if (l1 != l2) {
+
+				while (l1 != e[l1].equivalent) {
+				    l1 = e[l1].equivalent;
+				}
+				if (l1 != l2) {
+				    if (e[l1].val_of_min < e[l2].val_of_min
+					|| (e[l1].val_of_min == e[l2].val_of_min
+					    && e[l1].area >
+					    e[l2].area)) {
+					/* cur basin absorbs nb basin */
+					e[l2].vol_ex_val = current_level - e[l2].val_of_min;
+					e[l1].vol += e[l2].vol;
+					e[l1].area += e[l2].area;
+					e[l2].equivalent = l1;
+				    }
+				    else {
+					/* nb basin absorbs cur basin */
+					e[l1].vol_ex_val = current_level - e[l1].val_of_min;
+					e[l2].vol += e[l1].vol;
+					e[l2].area += e[l1].area;
+					e[l1].equivalent = l2;
+					l1 = l2;
+				    }
+				}
+			    }
+			}
+			else if (l2 == 0) {	// Add it to the tmp offsets queue
+			    tmpOffsets.push_back (nbOffset);
+			    lblPixels[nbOffset] = nbr_label + 1;
+			}
+		    }
+		}
+	    if (!tmpOffsets.empty ()) {
+		typename vector < UINT >::iterator t_it = tmpOffsets.begin ();
+
+		while (t_it != tmpOffsets.end ()) {
+		    hq.push (inPixels[*t_it], *t_it);
+		    t_it++;
+		}
+	    }
+	    tmpOffsets.clear ();
+	}
+
+	// Update Last level of flooding.
+	e[lblPixels[curOffset]].vol += e[lblPixels[curOffset]].area;
+	e[lblPixels[curOffset]].vol_ex_val += 1; // the last minimum has the largest dynamics. It would be better if we knew minVal and maxVal
+
+	return RES_OK;
+    }
 
     /**
      * Watershed using volume extinction values
@@ -262,6 +530,7 @@ namespace smil
 						&imMarkers,
 						Image < outT > &imOut,
 						Image < labelT > &imBasinsOut,
+						const char *  	extinctionType="v", 
 						const StrElt & se =
 						DEFAULT_SE) {
 	ASSERT_ALLOCATED (&imIn, &imMarkers, &imOut, &imBasinsOut);
@@ -279,8 +548,23 @@ namespace smil
 	HierarchicalQueue < T > pq;
 	initWatershedHierarchicalQueueExtinction (imIn, imBasinsOut,
 						  pq, e, nbr_label);
-	processWatershedHierarchicalQueueExtinction (imIn, imBasinsOut,
+	if(strcmp(extinctionType, "v")==0){
+	  processVolWatershedHierarchicalQueueExtinction (imIn, imBasinsOut,
 						     pq, se, e, nbr_label);
+	}
+	else if(strcmp(extinctionType,"d")==0){
+	  processDynWatershedHierarchicalQueueExtinction (imIn, imBasinsOut,
+						     pq, se, e, nbr_label);
+
+	}
+	else if(strcmp(extinctionType, "a")==0){
+	  processAreaWatershedHierarchicalQueueExtinction (imIn, imBasinsOut,
+						     pq, se, e, nbr_label);
+	}
+	else{
+	  return RES_ERR;
+	}
+	
 
 	typename ImDtTypes < outT >::lineType pixOut = imOut.getPixels ();
 	typename ImDtTypes < labelT >::lineType pixMarkers = imMarkers.getPixels ();
@@ -316,16 +600,18 @@ namespace smil
 	class outT > RES_T watershedExtinction (const Image < T > &imIn,
 						Image < labelT > &imMarkers,
 						Image < outT > &imOut,
+						const char *  	extinctionType="v", 
 						const StrElt & se =
 						DEFAULT_SE) {
 	ASSERT_ALLOCATED (&imIn, &imMarkers, &imOut);
 	ASSERT_SAME_SIZE (&imIn, &imMarkers, &imOut);
 	Image < labelT > imBasinsOut (imMarkers);
-	return watershedExtinction (imIn, imMarkers, imOut, imBasinsOut, se);
+	return watershedExtinction (imIn, imMarkers, imOut, imBasinsOut, extinctionType, se);
     }
     template < class T,
 	class outT > RES_T watershedExtinction (const Image < T > &imIn,
 						Image < outT > &imOut,
+						const char *  	extinctionType="v", 
 						const StrElt & se =
 						DEFAULT_SE) {
 	ASSERT_ALLOCATED (&imIn, &imOut);
@@ -334,7 +620,7 @@ namespace smil
 	minima (imIn, imMin, se);
 	Image < UINT > imLbl (imIn);
 	label (imMin, imLbl, se);
-	return watershedExtinction (imIn, imLbl, imOut, se);
+	return watershedExtinction (imIn, imLbl, imOut,extinctionType,  se);
     }
     template < class T, class labelT, class outT,
 	class HQ_Type >
