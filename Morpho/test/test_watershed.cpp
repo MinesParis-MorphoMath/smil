de265b604155e399baee7bb461f0a73262e3c61c
Matthieu Faessel <faessel@cmm.ensmp.fr>
2014-09-29 17:55:40 +0200

    start

--- Morpho/test/test_watershed.cpp
+++ Morpho/test/test_watershed.cpp
@@ -477,15 +477,291 @@ class Test_Build : public TestCase
   }
 };
 
-struct A
+// template <class T>
+// class FloodingBasin
+// {
+//   public:
+//     virtual void test() { cout << "FloodingBasin" << endl; }
+//     virtual FloodingBasin *clone() = 0;
+//     virtual void initValue(const T &val) {}
+//     virtual void increment(const T &val=1) {}
+//     virtual void raise(const T &val) {}
+//     virtual void merge(FloodingBasin<T> *other) {}
+//     virtual void finalize() {}
+//     UINT label;
+// };
+// 
+// template <class T>
+// class VolumeFloodingBasin : public FloodingBasin<T>
+// {
+//   public:
+//     virtual void test() { cout << "Volume FloodingBasin" << endl; }
+//     virtual VolumeFloodingBasin *clone() { return new VolumeFloodingBasin; }
+//     virtual void initValue(const T &val) {}
+//     virtual void increment(const T &val=1) {}
+//     virtual void raise(const T &val) {}
+//     virtual void merge(FloodingBasin<T> *other) {}
+//     virtual void finalize() {}
+//     UINT label;
+// };
+
+template <class T>
+class Flooding
 {
-    virtual void test() { cout << "A" << endl; }
+  public:
+    Flooding()
+      : basinNbr(0)
+    {
+    }
+    virtual ~Flooding()
+    {
+	deleteBasins();
+	delete aType;
+    }
+    void setBasinType(FloodingBasin<T> *a)
+    {
+	aType = a->clone();
+    }
+    
+    virtual UINT merge(UINT &lbl1, UINT &lbl2) {}
+    
+    template <class labelT>
+    RES_T flood(const Image<T> &imIn, Image<labelT> &imLbl, const StrElt & se)
+    {
+	ASSERT_ALLOCATED (&imIn, &imLbl);
+	ASSERT_SAME_SIZE (&imIn, &imLbl);
+// 	ImageFreezer freezer (imOut);
+// 	ImageFreezer freezer2 (imBasinsOut);
+
+// 	copy (imMarkers, imBasinsOut);
+
+	UINT nbr_label = maxVal (imLbl);
+	createBasins(nbr_label + 1);
+
+/*
+	HierarchicalQueue < T > pq;
+	initWatershedHierarchicalQueueExtinction(imIn, imBasinsOut, pq, e, nbr_label);
+	processWatershedExtinctionHierarchicalQueue<T,labelT,HierarchicalQueue<T>,outT,basinT> (imIn, imBasinsOut, pq, se, e, nbr_label);
+	
+	typename ImDtTypes < outT >::lineType pixOut = imOut.getPixels ();
+	typename ImDtTypes < labelT >::lineType pixMarkers = imMarkers.getPixels ();
+
+	fill (imOut, outT(0));
+	// Create the image containing the ws lines
+	T wsVal = ImDtTypes < outT >::max ();
+	size_t max_ext=0;
+	for (int i=0; i<nbr_label; ++i) {
+		if (max_ext<e[i].extinctionValue) max_ext = e[i].extinctionValue ;
+	}
+	basinT *e_cpy = new basinT[nbr_label+1]; 
+	memcpy (e_cpy, e, (nbr_label+1)*sizeof(basinT));
+	for (int i=1; i<nbr_label+1; ++i)
+		e_cpy[i].label = i;
+	vector<basinT> ve (e_cpy+1, e_cpy+nbr_label+1);
+	// Sorting the vol_ev_val.
+	sort (ve.begin(), ve.end());
+	for (int i=0; i<nbr_label; ++i){
+		e[ve[i].label].extinctionValue = nbr_label - i;
+	}
+	delete[]e_cpy;
+	for (size_t i=0; i<imIn.getPixelCount (); i++, *pixMarkers++, pixOut++) {
+		if(*pixMarkers != labelT(0))
+			*pixOut = e[*pixMarkers].extinctionValue ;
+	}
+
+	delete[]e;
+	return RES_OK;
+	
+	*/
+	initHierarchicalQueue(imIn, imLbl);
+	processHierarchicalQueue(imIn, imLbl, se);
+    }
+    
+  protected:
+    FloodingBasin<T> *aType;
+    vector<FloodingBasin<T>*> basins;
+    UINT basinNbr;
+    HierarchicalQueue < T > hq;
+    T currentLevel;
+    
+    void createBasins(UINT nbr)
+    {
+	if (nbr==basinNbr)
+	  return;
+	
+	if (basinNbr!=0)
+	  deleteBasins();
+	
+	for (UINT i=0;i<nbr;i++)
+	{
+	    FloodingBasin<T> *_basin = aType->clone();
+	    basins.push_back(aType->clone());
+	}
+    }
+    void deleteBasins()
+    {
+	if (basinNbr==0)
+	  return;
+	
+	for (UINT i=0;i<basinNbr;i++)
+	  delete basins[i];
+	basins.clear();
+	basinNbr = 0;
+    }
+    
+    template <class labelT>
+    void initHierarchicalQueue(const Image<T> &imIn, Image<labelT> &imLbl)
+    {
+	for (UINT i = 0; i < basinNbr + 1; ++i)
+	  basins[i]->label = i;
+
+	// Empty the priority queue
+	hq.initialize (imIn);
+	typename ImDtTypes < T >::lineType inPixels = imIn.getPixels ();
+	typename ImDtTypes < labelT >::lineType lblPixels = imLbl.getPixels ();
+	size_t s[3];
+
+	imIn.getSize (s);
+	size_t offset = 0;
+
+	for (size_t k = 0; k < s[2]; k++)
+	    for (size_t j = 0; j < s[1]; j++)
+		for (size_t i = 0; i < s[0]; i++) {
+		    if (*lblPixels != 0) {
+			hq.push (T (*inPixels), offset);
+			basins[*lblPixels]->initValue(*inPixels);  
+			basins[*lblPixels]->increment(*inPixels);  
+		    }
+
+		    inPixels++;
+		    lblPixels++;
+		    offset++;
+		}
+    }
+    
+    template < class labelT >
+    RES_T processHierarchicalQueue (const Image < T > &imIn,
+							   Image < labelT > &imLbl,
+							   const StrElt & se)
+    {
+	typename ImDtTypes < T >::lineType inPixels = imIn.getPixels ();
+	typename ImDtTypes < labelT >::lineType lblPixels =
+	    imLbl.getPixels ();
+	vector < int >dOffsets;
+
+	vector < IntPoint >::const_iterator it_start = se.points.begin ();
+	vector < IntPoint >::const_iterator it_end = se.points.end ();
+	vector < IntPoint >::const_iterator it;
+	vector < UINT > tmpOffsets;
+	size_t s[3];
+
+	imIn.getSize (s);
+
+	T curPixVal;
+	currentLevel = 0;
+	size_t curOffset;
+
+	// set an offset distance for each se point
+	for (it = it_start; it != it_end; it++) 
+	{
+	    dOffsets.push_back (it->x + it->y * s[0] + it->z * s[0] * s[1]);
+	}
+	vector < int >::iterator it_off_start = dOffsets.begin ();
+	vector < int >::iterator it_off;
+
+	while (!hq.isEmpty ()) 
+	{
+	    curOffset = hq.pop ();
+	    curPixVal = inPixels[curOffset];
+
+	    // Rising the elevation of the basins
+	    if (curPixVal > currentLevel) 
+	    {
+		currentLevel = curPixVal;
+		for (labelT i = 1; i < basinNbr + 1; ++i) 
+			basins[i]->raise(currentLevel);
+	    }
+
+	    size_t x0, y0, z0;
+
+	    imIn.getCoordsFromOffset (curOffset, x0, y0, z0);
+	    bool oddLine = se.odd && ((y0) % 2);
+	    int x, y, z;
+	    size_t nbOffset;
+	    UINT l1 = lblPixels[curOffset], l2;
+
+	    for (it = it_start, it_off = it_off_start; it != it_end; it++, it_off++)
+		if (it->x != 0 || it->y != 0 || it->z != 0)	// useless if x=0 & y=0 & z=0
+		{
+		    x = x0 + it->x;
+		    y = y0 + it->y;
+		    z = z0 + it->z;
+		    if (oddLine)
+			x += (((y + 1) % 2) != 0);
+		    if (x >= 0 && x < (int) s[0] && y >= 0 && y < (int) s[1]
+			&& z >= 0 && z < (int) s[2]) 
+		    {
+			nbOffset = curOffset + *it_off;
+			if (oddLine)
+			    nbOffset += (((y + 1) % 2) != 0);
+			l2 = lblPixels[nbOffset];
+			if (l2 > 0 && l2 != basinNbr + 1) 
+			{
+			    while (l2 != basins[l2]->label) 
+			    {
+				l2 = basins[l2]->label;
+			    }
+
+			    if (l1 == 0 || l1 == basinNbr + 1) 
+			    {
+				l1 = l2; // current pixel takes the label of its first labelled ngb  found
+				lblPixels[curOffset] = l1;
+				basins[l1]->increment();
+			    }
+			    else if (l1 != l2) 
+			    {
+				while (l1 != basins[l1]->label) 
+				{
+				    l1 = basins[l1]->label;
+				}
+				if (l1 != l2) 
+				  merge(l1, l2);
+			    }
+			}
+			else if (l2 == 0) 	// Add it to the tmp offsets queue
+			{
+			    tmpOffsets.push_back (nbOffset);
+			    lblPixels[nbOffset] = basinNbr + 1;
+			}
+		    }
+		}
+	    if (!tmpOffsets.empty ()) 
+	    {
+		typename vector < UINT >::iterator t_it = tmpOffsets.begin ();
+
+		while (t_it != tmpOffsets.end ()) 
+		{
+		    hq.push (inPixels[*t_it], *t_it);
+		    t_it++;
+		}
+	    }
+	    tmpOffsets.clear ();
+	}
+
+	// Update Last level of flooding.
+	basins[lblPixels[curOffset]]->finalize();
+
+	return RES_OK;
+    }
 };
 
-struct B
+
+template <class T>
+struct Flooding_B : public Flooding<T>
 {
-    virtual void test() { cout << "B" << endl; }
 };
+
+
 
 int main(int argc, char *argv[])
 {
@@ -497,6 +773,32 @@ int main(int argc, char *argv[])
       ADD_TEST(ts, Test_Watershed_Extinction);
 //       ADD_TEST(ts, Test_Watershed_Extinction_Graph);
       ADD_TEST(ts, Test_Build);
+      
+      Flooding_B<UINT8> fb;
+      VolumeFloodingBasin<UINT8> b;
+      fb.setBasinType(&b);
+      
+      UINT8 vecIn[] = {
+	  2,    2,    2,    2,    2,
+	    3,    2,    5,    9,    5,
+	  3,    3,    9,    0,    0,
+	    1,    1,    9,    0,    0,
+	  1,    1,    9,    0,    0,
+      };
+      UINT8 vecMark[] = {
+	  0,    1,    0,    0,    0,
+	    0,    0,    0,    0,    0,
+	  0,    0,    0,    0,    0,
+	    0,    2,    0,    3,    0,
+	  0,    2,    0,    0,    3,
+      };
+      StrElt se = sSE();
+
+      Image_UINT8 imIn (5,5) ;
+      Image_UINT8 imMark (imIn) ;
+      
+      fb.flood(imIn, imMark, hSE());
+      fb[0]->test();
       
       return ts.run();
       
