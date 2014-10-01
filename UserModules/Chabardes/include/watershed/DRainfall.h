25ff45c114bcfa097353d01c443f3bb4cd531a3c
chabardes <chabardes@chabardesAlien>
2014-10-01 22:23:03 +0200

    Fix and Test arrowMin, wrote small hammingWeight function to count problematic pixel in arrow image.

--- UserModules/Chabardes/include/watershed/DRainfall.h
+++ UserModules/Chabardes/include/watershed/DRainfall.h
@@ -1,5 +1,14 @@
 #ifndef _DRAINFALL_H_
 #define _DRAINFALL_H_
 
+namespace smil
+{
+
+
+template <class T, class labelT, class arrowT> 
+RES_T waterdrop (const Image<T> &in, const Image<arrowT> &arrow, size_t offset, Image<labelT>, const StrElt &se) {
+	
+}
+}
 
 #endif // _DRAINFALL_H_
