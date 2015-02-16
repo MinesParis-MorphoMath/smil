b6d143d60486fec3ee3266529fb1c3210cc96d26
Matthieu Faessel <faessel@cmm.ensmp.fr>
2015-02-16 16:45:20 +0100

    Move Chabardes to external repo

--- UserModules/Chabardes/include/DChabardes.h
+++
@@ -1,8 +0,0 @@
-#ifndef _DCHABARDES_H_
-#define _DCHABARDES_H_
-
-//#include "DDistributed.h"
-#include "DWatershed.h"
-
-#endif // _DCHABARDES_H_
-
--- UserModules/Chabardes/include/DWatershed.h
+++
@@ -1,9 +0,0 @@
-#ifndef _DWATERSHED_H_
-#define _DWATERSHED_H_
-
-#include "Morpho/include/DMorpho.h"
-
-#include "private/DMinima.h"
-#include "private/DArrow.h"
-
-#endif // _DWATERSHED_H_
