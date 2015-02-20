2211bf87e3a96bbc3a88af375a71dc0d9c451016
Matthieu Faessel <faessel@cmm.ensmp.fr>
2015-02-20 15:38:02 +0100

    Modify OpenCV test.

--- Addons/OpenCV/test/test_opencv.cpp
+++ Addons/OpenCV/test/test_opencv.cpp
@@ -35,34 +35,6 @@ class Test_SmilToCv : public TestCase
 };
 
 
-#ifdef WRAP_PYTHON
-    class Test_Python_Import : public TestCase
-    {
-        virtual void run()
-        {
-          Py_Initialize();
-
-          PyObject *_main = PyImport_ImportModule("__main__");
-          PyObject *globals = PyModule_GetDict(_main);
-          
-          PyRun_String("import sys", Py_file_input, globals, NULL);
-          PyRun_SimpleString((string("sys.path.append(\"") + MORPHEE_LIBRARY_DIR + "\")").c_str());
-          PyRun_SimpleString("import MorpheePython as mp");
-          PyRun_SimpleString("mIm = mp.createImage(mp.dataCategory.dtScalar, mp.scalarDataType.sdtUINT8)");
-          PyRun_SimpleString("mIm.setSize(256,256)");
-          PyRun_SimpleString("mIm.setColorInfo(mp.colorInfo.ciMonoSpectral)");
-          PyRun_SimpleString("mIm.allocateImage()");
-
-          PyObject *pyobj = PyDict_GetItem(globals, PyString_FromString( "mIm" ));
-
-          OpenCVInt<UINT8> mIm(pyobj);
-          TEST_ASSERT(mIm.isAllocated());
-          
-          Py_Finalize();
-            
-        }
-    };
-#endif // WRAP_PYTHON
 
 int main(int argc, char *argv[])
 {
@@ -70,9 +42,6 @@ int main(int argc, char *argv[])
       
       ADD_TEST(ts, Test_CvToSmil);
       ADD_TEST(ts, Test_SmilToCv);
-#ifdef WRAP_PYTHON
-      ADD_TEST(ts, Test_Python_Import);
-#endif // WRAP_PYTHON
       
       return ts.run();
   
