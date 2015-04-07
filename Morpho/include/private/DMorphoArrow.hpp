8c252530efbbe09517c531fd8aab77162c66a664
Théodore Chabardès <theodore.chabardes@mines-paristech.fr>
2015-04-07 09:47:16 +0200

    Modified Arrow (Optimisations)

--- Morpho/include/private/DMorphoArrow.hpp
+++ Morpho/include/private/DMorphoArrow.hpp
@@ -282,9 +282,9 @@ namespace smil
                             if (oddLine)
                                 x += (y+1)%2;
                             nb_o = x + y*size[0] + z*size[1]*size[0];
-                            if (x >= 0 && x < size[0] &&
-                                y >= 0 && y < size[1] && 
-                                z >= 0 && z < size[2]) 
+                            if ( x < size[0] &&
+                                 y < size[1] && 
+                                 z < size[2]) 
                                 if (testAndAssign (statutP[nb_o], outP[nb_o]))
                                     q.push(nb_o);
                        }
@@ -303,6 +303,312 @@ namespace smil
             }
     };
 
+    template < class T1,
+	class T2 > struct equSupLine3:public tertiaryLineFunctionBase < T1 >
+    {
+	equSupLine3 (  ):trueVal ( ImDtTypes < T2 >::max (  ) ),
+	    falseVal ( 0 )
+	{
+	}
+
+	typedef typename Image < T1 >::lineType lineType1;
+	typedef typename Image < T2 >::lineType lineType2;
+
+	T2 trueVal, falseVal;
+
+	inline void operator      (  ) ( const lineType1 lIn1,
+					 const lineType1 lIn2,
+					 const size_t size, lineType2 lOut )
+	{
+	    return _exec ( lIn1, lIn2, size, lOut );
+	}
+	inline void _exec ( const lineType1 lIn1,
+			    const lineType1 lIn2,
+			    const size_t size, lineType2 lOut )
+	{
+	    T2 _trueVal ( trueVal ), _falseVal ( falseVal );
+
+	    for ( size_t i = 0; i < size; i++ )
+	      {
+		  lOut[i] += ( lIn1[i] == lIn2[i] ) ? _trueVal : _falseVal;
+	      }
+	}
+    };
+
+
+    template < class T, class arrowT = UINT8 > 
+    class arrowMinFunction : public MorphImageFunctionBase<T, arrowT>
+    {
+      public:
+	typedef MorphImageFunctionBase < T, arrowT > parentClass;
+	typedef typename parentClass::imageInType imageInType;
+	typedef typename imageInType::lineType lineInType;
+	typedef typename imageInType::lineType sliceInType;
+	typedef typename imageInType::volType volInType;
+	typedef typename parentClass::imageOutType imageArrowType;
+	typedef typename imageArrowType::lineType lineArrowType;
+	typedef typename imageArrowType::sliceType sliceArrowType;
+	typedef typename imageArrowType::volType volArrowType;
+
+	arrowMinFunction ( T border = numeric_limits < T >::max (  ) ):borderValue ( border ), MorphImageFunctionBase < T, arrowT > (  )
+	{
+	    borderValue = border;
+	}
+
+	virtual RES_T _exec ( const imageInType & in,
+			      imageArrowType & imOut, const StrElt & se );
+      private:
+	T borderValue;
+	size_t lineLen;
+
+    };
+
+    template < class T, class arrowT >
+    RES_T arrowMinFunction < T, arrowT >::_exec ( const imageInType & in,
+						  imageArrowType & arrow,
+						  const StrElt & se )
+    {
+	ASSERT_ALLOCATED ( &in, &arrow );
+	ASSERT_SAME_SIZE ( &in, &arrow );
+
+	if ( !areAllocated ( &in, &arrow, NULL ) )
+	    return RES_ERR_BAD_ALLOCATION;
+
+	StrElt cpSe = se.noCenter ();
+
+	UINT sePtsNumber = cpSe.points.size (  );
+
+	if ( sePtsNumber == 0 )
+	    return RES_OK;
+
+	size_t nSlices = in.getSliceCount (  );
+	size_t nLines = in.getHeight (  );
+
+	lineLen = in.getWidth (  );
+
+	volInType srcSlices = in.getSlices (  );
+	volArrowType destSlices = arrow.getSlices (  );
+	lineInType *srcLines;
+	lineArrowType *destLines;
+
+	bool oddSe = cpSe.odd, oddLine = 0;
+
+	#pragma omp parallel private(oddLine)
+	{
+	T* borderBuf = ImDtTypes < T >::createLine ( lineLen );
+	T* cpBuf = ImDtTypes < T >::createLine ( lineLen );
+	T* minBuf = ImDtTypes < T >::createLine ( lineLen );
+	T* nullBuf = ImDtTypes < T >::createLine ( lineLen );
+	T* flagBuf = ImDtTypes < T >::createLine ( lineLen );
+ 	lowLine < T > low;
+	infLine < T > inf;
+	testLine < T, arrowT > test;
+	equSupLine3 < T, arrowT > equSup;
+	equLine < T > equ;
+
+	fillLine < T > ( nullBuf, lineLen, arrowT ( 0 ) );
+	fillLine < T > ( borderBuf, lineLen, T ( borderValue ) );
+	equ.trueVal = 0;
+	equ.falseVal = numeric_limits < T >::max (  );
+
+	lineInType lineIn;
+	lineArrowType lineArrow;
+	size_t x, y, z;
+
+	for ( size_t s = 0; s < nSlices; ++s )
+        {
+	  srcLines = srcSlices[s];
+	  destLines = destSlices[s];
+
+	  #pragma omp for
+	  for ( size_t l = 0; l < nLines; ++l )
+	  {
+	        oddLine = oddSe && l %2;
+
+		lineIn = srcLines[l];
+		lineArrow = destLines[l];
+
+		fillLine < arrowT > ( lineArrow, lineLen, arrowT ( 0 ) );
+		copyLine < T > ( lineIn, lineLen, minBuf );
+
+		for ( UINT p = 0; p < sePtsNumber; ++p )
+	        {
+		    y = l + cpSe.points[p].y;
+		    x = -cpSe.points[p].x - (oddLine && (y+1)%2);
+		    z = s + cpSe.points[p].z;
+
+		    equSup.trueVal = ( 1UL << p );
+
+
+  		    if ( z >= nSlices || y >= nLines )
+		        copyLine < T > ( borderBuf, lineLen, cpBuf );
+		    else
+		        shiftLine < T > ( srcLines[y], x, lineLen, cpBuf, borderValue );
+
+		    low._exec ( cpBuf, minBuf, lineLen, flagBuf );
+		    inf._exec ( cpBuf, minBuf, lineLen, minBuf );
+		    test._exec ( flagBuf, nullBuf, lineArrow, lineLen,
+			       lineArrow );
+		    equSup._exec ( cpBuf, minBuf, lineLen, lineArrow );
+
+		}
+	    }
+	}
+	}
+
+	return RES_OK;
+    };
+
+    template < class T > RES_T arrowMin ( const Image < T > &im,
+							Image < T >
+							&arrow,
+							const StrElt & se,
+							T borderValue =
+							numeric_limits <
+							T >::max (  ) )
+    {
+	arrowMinFunction < T, T > iFunc ( borderValue );
+	return iFunc ( im, arrow, se );
+    }
+
+    template < class T, class arrowT = UINT8 > 
+    class arrowMinStepFunction : public MorphImageFunctionBase<T, arrowT>
+    {
+      public:
+	typedef MorphImageFunctionBase < T, arrowT > parentClass;
+	typedef typename parentClass::imageInType imageInType;
+	typedef typename imageInType::lineType lineInType;
+	typedef typename imageInType::lineType sliceInType;
+	typedef typename imageInType::volType volInType;
+	typedef typename parentClass::imageOutType imageArrowType;
+	typedef typename imageArrowType::lineType lineArrowType;
+	typedef typename imageArrowType::sliceType sliceArrowType;
+	typedef typename imageArrowType::volType volArrowType;
+
+	arrowMinStepFunction ( T border = numeric_limits < T >::max (  ) ):borderValue ( border ), MorphImageFunctionBase < T, arrowT > (  )
+	{
+	    borderValue = border;
+	}
+
+	virtual RES_T _exec ( const imageInType & in,
+			      imageArrowType & imOut, const StrElt & se );
+      private:
+	T borderValue;
+	size_t lineLen;
+
+    };
+
+    template < class T, class arrowT >
+    RES_T arrowMinStepFunction < T, arrowT >::_exec ( const imageInType & in,
+						  imageArrowType & arrow,
+						  const StrElt & se )
+    {
+	ASSERT_ALLOCATED ( &in, &arrow );
+	ASSERT_SAME_SIZE ( &in, &arrow );
+
+	if ( !areAllocated ( &in, &arrow, NULL ) )
+	    return RES_ERR_BAD_ALLOCATION;
+
+	StrElt cpSe = se.noCenter ();
+
+	UINT sePtsNumber = cpSe.points.size (  );
+
+	if ( sePtsNumber == 0 )
+	    return RES_OK;
+
+	size_t nSlices = in.getSliceCount (  );
+	size_t nLines = in.getHeight (  );
+	lineLen = in.getWidth (  );
+
+	volInType srcSlices = in.getSlices (  );
+	volArrowType destSlices = arrow.getSlices (  );
+	lineInType *srcLines;
+	lineArrowType *destLines;
+
+	bool oddSe = cpSe.odd, oddLine = 0;
+
+	#pragma omp parallel private(oddLine)
+	{
+	T* borderBuf = ImDtTypes < T >::createLine ( lineLen );
+	T* cpBuf = ImDtTypes < T >::createLine ( lineLen );
+	T* minBuf = ImDtTypes < T >::createLine ( lineLen );
+	T* nullBuf = ImDtTypes < T >::createLine ( lineLen );
+	T* flagBuf = ImDtTypes < T >::createLine ( lineLen );
+ 	lowLine < T > low;
+	infLine < T > inf;
+	testLine < T, arrowT > test;
+	equSupLine3 < T, arrowT > equSup;
+	equLine < T > equ;
+
+	fillLine < T > ( nullBuf, lineLen, arrowT ( 0 ) );
+	fillLine < T > ( borderBuf, lineLen, T ( borderValue ) );
+	equ.trueVal = 0;
+	equ.falseVal = numeric_limits < T >::max (  );
+
+	lineInType lineIn;
+	lineArrowType lineArrow;
+	size_t x, y, z, i;
+
+	for ( size_t s = 0; s < nSlices; ++s )
+        {
+	  srcLines = srcSlices[s];
+	  destLines = destSlices[s];
+
+	  #pragma omp for
+	  for ( size_t l = 0; l < nLines; ++l )
+	  {
+	        oddLine = oddSe && l %2;
+
+		lineIn = srcLines[l];
+		lineArrow = destLines[l];
+
+		fillLine < arrowT > ( lineArrow, lineLen, arrowT ( 0 ) );
+		copyLine < T > ( borderBuf, lineLen, minBuf );
+
+		for ( UINT p = 0; p < sePtsNumber; ++p )
+	        {
+		    y = l + cpSe.points[p].y;
+		    x = -cpSe.points[p].x - (oddLine && (y+1)%2);
+		    z = s + cpSe.points[p].z;
+
+		    equSup.trueVal = ( 1UL << p );
+
+
+  		    if ( z >= nSlices || y >= nLines )
+		        copyLine < T > ( borderBuf, lineLen, cpBuf );
+		    else
+		        shiftLine < T > ( srcLines[y], x, lineLen, cpBuf, borderValue );
+
+		    for (i=0; i<lineLen; ++i)
+			flagBuf[i] = cpBuf[i] >= lineIn[i] ? 255 : 0;
+		    for (i=0; i<lineLen; ++i)
+			cpBuf[i] = flagBuf[i] ? cpBuf[i] : borderBuf[i];
+
+		    low._exec ( cpBuf, minBuf, lineLen, flagBuf );
+		    inf._exec ( cpBuf, minBuf, lineLen, minBuf );
+		    test._exec ( flagBuf, nullBuf, lineArrow, lineLen,
+			       lineArrow );
+		    equSup._exec ( cpBuf, minBuf, lineLen, lineArrow );
+
+		}
+	    }
+	}
+	}
+
+	return RES_OK;
+    };
+
+    template < class T > RES_T arrowMinStep ( const Image < T > &im,
+							Image < T >
+							&arrow,
+							const StrElt & se,
+							T borderValue =
+							numeric_limits <
+							T >::max (  ) )
+    {
+	arrowMinStepFunction < T, T > iFunc ( borderValue );
+	return iFunc ( im, arrow, se );
+    }
 
 
 /** \} */
