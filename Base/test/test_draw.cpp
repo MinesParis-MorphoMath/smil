4677730a66de9831322e604db90c7aed77c63fea
Pierre Guillou <guillou@cri.ensmp.fr>
2018-06-15 14:51:02 +0200

    Base/test_draw: Search DejaVu font in several places
    This test hardcodes the path of the DejaVu font used for drawing, but
    the path used is OS dependant and does not work at least on ArchLinux.
    
    To be backward-compatible with C++<11, I used C arrays and the
    ifstream C++ class, instead of the C++11 std::array and the new C++17
    std::filesystem APIs.

--- Base/test/test_draw.cpp
+++ Base/test/test_draw.cpp
@@ -24,6 +24,10 @@
 #include "Core/include/DCore.h"
 #include "DImageDraw.hpp"
 
+#ifdef USE_FREETYPE
+#include <fstream>
+#endif // USE_FREETYPE
+
 using namespace smil;
 
 
@@ -119,9 +123,26 @@ class Test_DrawText : public TestCase
       Image_UINT8 im1(20,15);
       Image_UINT8 im2(20,15);
       fill(im1, UINT8(127));
-      
+
+#define N_FONT_PATHS 3
+      string font_search_paths[N_FONT_PATHS] = {
+          "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", // Ubuntu
+          "/usr/share/fonts/TTF/DejaVuSans.ttf",             // ArchLinux
+          "/usr/share/fonts/dejavu/DejaVuSans.ttf",          // CentOS
+      };
+      string fpath;
+      for (int i = 0; i < N_FONT_PATHS; i++) {
+        fpath = font_search_paths[i];
+        // trying to open the font path to check if it exists
+        ifstream res(fpath.c_str());
+        if (res.good()) {
+          res.close();
+          break;
+        }
+      }
+
       // JOE TEST_ASSERT(drawText(im1, 4, 10, "ok", "/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf", 10)==RES_OK);
-      TEST_ASSERT(drawText(im1, 4, 10, "ok", "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 10)==RES_OK);
+      TEST_ASSERT(drawText(im1, 4, 10, "ok", fpath, 10) == RES_OK);
 
       UINT8 vec[20*15] = 
       {   
@@ -159,8 +180,12 @@ int main(void)
 
       ADD_TEST(ts, Test_DrawCircle);
       ADD_TEST(ts, Test_DrawSphere);
+#ifdef __linux__
+      // The following test uses the DejaVu font through hard-coded
+      // Linux paths only
       ADD_TEST(ts, Test_DrawText);
-      
+      // TODO: Make it OS-independent?
+#endif
+
       return ts.run();
 }
-
