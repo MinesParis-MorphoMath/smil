a0bb29e041257da8037565f4e5578369bfa7df20
Pierre Guillou <guillou@cri.ensmp.fr>
2018-07-12 11:13:40 +0200

    IO/test_IO.cpp: Add more tests around image formats read/write
    Add testing other image formats than PNG, such as TIFF, JPEG and PGM.
    Add some TEST_ASSERT macros to check the results.
    Also test the createFromFile() function (called by Python).

--- IO/test/test_IO.cpp
+++ IO/test/test_IO.cpp
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2011-2015, Matthieu FAESSEL and ARMINES
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *
@@ -14,33 +14,25 @@
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND CONTRIBUTORS BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
-
-#include <cstdio>
-#include <ctime>
-
-
 #include "Core/include/DCore.h"
-// #include "DIO.h"
 #include "IO/include/private/DImageIO_RAW.hpp"
-#include "Gui/include/DGui.h"
 
-#include "Core/include/DColor.h"
 #ifdef SMIL_WRAP_RGB
 #include "NSTypes/RGB/include/DRGB.h"
 #endif // SMIL_WRAP_RGB
-
 
 using namespace smil;
 
@@ -50,51 +42,138 @@ class Test_RW_RAW : public TestCase
   {
     typedef UINT8 T;
     const char *fName = "_smil_io_tmp.raw";
-    
+
     Image<T> im1(3, 3, 2);
-    T tab[] = { 28, 2, 3,
-                 2, 5, 6,
-                 3, 8, 9,
-                 4, 11, 12,
-                 5, 15, 16,
-                 6, 18, 19 };
+    T tab[] = {
+      28, 2, 3,
+      2, 5, 6,
+      3, 8, 9,
+      4, 11, 12,
+      5, 15, 16,
+      6, 18, 19
+    };
     im1 << tab;
-    TEST_ASSERT( writeRAW(im1, fName)==RES_OK );
-    
+    TEST_ASSERT(writeRAW(im1, fName) == RES_OK);
+
     Image<T> im2;
-    
-    TEST_ASSERT( readRAW(fName, 3,3,2, im2)==RES_OK );
-    
-    TEST_ASSERT(im1==im2);
+    TEST_ASSERT(readRAW(fName, 3, 3, 2, im2) == RES_OK);
+    TEST_ASSERT(im1 == im2);
   }
 };
 
+#ifdef USE_PNG
+class Test_RW_PNG : public TestCase
+{
+  virtual void run()
+  {
+    const char *fName = "_smil_io_tmp.png";
+    Image<UINT8> im1(3, 3);
+    UINT8 pix[] = {28, 2, 3, 2, 5, 6, 3, 8, 9};
+    im1 << pix;
+    TEST_ASSERT(write(im1, fName) == RES_OK);
+    Image<UINT8> im2;
+    TEST_ASSERT(read(fName, im2) == RES_OK);
+    TEST_ASSERT(im1 == im2);
+    BaseImage *im3 = createFromFile(fName);
+    TEST_ASSERT(im3 != NULL);
+    delete im3;
+  }
+};
+
+#ifdef USE_CURL
+class Test_Curl : public TestCase
+{
+  virtual void run()
+  {
+    Image_UINT8 im1;
+
+    read("http://smil.cmm.mines-paristech.fr/images/barbara.png", im1);
+    TEST_ASSERT(im1.isAllocated());
+    Image_UINT8 im2("http://smil.cmm.mines-paristech.fr/images/barbara.png");
+    TEST_ASSERT(im2.isAllocated());
+    BaseImage *im0 =
+        createFromFile("http://smil.cmm.mines-paristech.fr/images/arearea.png");
+    TEST_ASSERT(im0 != NULL);
+    delete im0;
+  }
+};
+#endif // USE_CURL
+#endif // USE_PNG
+
+#ifdef USE_TIFF
+class Test_RW_TIFF : public TestCase
+{
+  virtual void run()
+  {
+    const char *fName = "_smil_io_tmp.tiff";
+    Image<UINT8> im1(3, 3);
+    UINT8 pix[] = {28, 2, 3, 2, 5, 6, 3, 8, 9};
+    im1 << pix;
+    TEST_ASSERT(write(im1, fName) == RES_OK);
+    Image<UINT8> im2;
+    TEST_ASSERT(read(fName, im2) == RES_OK);
+    TEST_ASSERT(im1 == im2);
+    BaseImage *im3 = createFromFile(fName);
+    TEST_ASSERT(im3 != NULL);
+    delete im3;
+  }
+};
+#endif // USE_TIFF
+
+// TODO: Test JPEG format
+
+class Test_RW_PGM : public TestCase
+{
+  virtual void run()
+  {
+    const char *fName = "_smil_io_tmp.pgm";
+    Image<UINT8> im1(3, 3);
+    UINT8 pix[] = {28, 2, 3, 2, 5, 6, 3, 8, 9};
+    im1 << pix;
+    TEST_ASSERT(write(im1, fName) == RES_OK);
+    Image<UINT8> im2;
+    TEST_ASSERT(read(fName, im2) == RES_OK);
+    TEST_ASSERT(im1 == im2);
+    BaseImage *im3 = createFromFile(fName);
+    TEST_ASSERT(im3 != NULL);
+    delete im3;
+  }
+};
+
+class Test_RW_BMP : public TestCase
+{
+  virtual void run()
+  {
+    const char *fName = "_smil_io_tmp.bmp";
+    Image<UINT8> im1(3, 3);
+    UINT8 pix[] = {28, 2, 3, 2, 5, 6, 3, 8, 9};
+    im1 << pix;
+    TEST_ASSERT(write(im1, fName) == RES_OK);
+    Image<UINT8> im2;
+    TEST_ASSERT(read(fName, im2) == RES_OK);
+    TEST_ASSERT(im1 == im2);
+    BaseImage *im3 = createFromFile(fName);
+    TEST_ASSERT(im3 != NULL);
+    delete im3;
+  }
+};
 
 int main(void)
 {
-    Image_UINT8 im1;
-    Image_UINT8 im2;
-    Image_UINT8 im3;
+  TestSuite ts;
 
-#if defined(USE_CURL) && defined(USE_PNG)
-    read("http://smil.cmm.mines-paristech.fr/images/barbara.png", im1);
-#endif // USE_CURL && USE_PNG
+  ADD_TEST(ts, Test_RW_RAW);
+#ifdef USE_PNG
+  ADD_TEST(ts, Test_RW_PNG);
+#ifdef USE_CURL
+  ADD_TEST(ts, Test_Curl);
+#endif // USE_CURL
+#endif // USE_PNG
+#ifdef USE_TIFF
+  ADD_TEST(ts, Test_RW_TIFF);
+#endif // USE_TIFF
+  ADD_TEST(ts, Test_RW_PGM);
+  ADD_TEST(ts, Test_RW_BMP);
 
-    
-#ifdef SMIL_WRAP_RGB
-    Image<RGB> rgbIm;
-#endif // SMIL_WRAP_RGB
-
-#if defined(USE_CURL) && defined(USE_PNG)
-    BaseImage *im0 = createFromFile("http://smil.cmm.mines-paristech.fr/images/arearea.png");
-    delete im0;
-#endif // USE_CURL && USE_PNG
-    
-    
-    TestSuite ts;
-
-    ADD_TEST(ts, Test_RW_RAW);
-    
-    return ts.run();
+  return ts.run();
 }
-
