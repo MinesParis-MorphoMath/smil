df4a088d7f98133f46f4a310e300a637f9aca73e
Jose-Marcio Martins da Cruz <Jose-Marcio.Martins@mines-paristech.fr>
2019-01-16 12:38:53 +0100

    Documentation of Addon modules 3DBilateralFilter and StochasticWatershed

--- Addons/StochasticWS/include/private/DStochasticWatershed.hpp
+++ Addons/StochasticWS/include/private/DStochasticWatershed.hpp
@@ -1,3 +1,32 @@
+/*
+ * Copyright (c) 2011-2016, Matthieu FAESSEL and ARMINES
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Matthieu FAESSEL, or ARMINES nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
 #ifndef _D_STOCHASTIC_CHABARDES_HPP_
 #define _D_STOCHASTIC_CHABARDES_HPP_
 
@@ -9,6 +38,12 @@
 
 namespace smil
 {
+    /**
+     * \ingroup Addons
+     * \defgroup StochasticWatershed Stochastic Watershed
+     * @{
+     */
+	
     template <class labelT, class T>
     class stochastic_edge
     {
@@ -661,6 +696,10 @@ namespace smil
         cut (weights, h, graph, weights.size()-1);
    }
 
+   /**
+    *  Over Segmentation Correction
+    *
+    */
    // Parallel.
    template <class labelT, class T>
    void stochasticWatershedParallel (const Image<labelT>& primary, const Image<T>& gradient, Image<labelT> &out, const size_t& n_seeds, const StrElt& s)
@@ -711,6 +750,10 @@ namespace smil
         stochasticGraphToPDF (primary, graph, out, s);
    }
 
+   /**
+    *  Over Segmentation Correction
+    *
+    */
    template <class labelT, class T>
    void stochasticWatershed (const Image<labelT>& primary, const Image<T>& gradient, Image<labelT> &out, const size_t& n_seeds, const StrElt& s)
    {
@@ -748,6 +791,10 @@ namespace smil
         stochasticGraphToPDF (primary, graph, out, s);
    }
 
+   /**
+    *  Over Segmentation Correction
+    *
+    */
    // Parallel
    template <class labelT, class T>
    size_t stochasticFlatZonesParallel (const Image<labelT>& primary, const Image<T>& gradient, Image<labelT> &out, const size_t& n_seeds, const double &t0, const StrElt& s)
@@ -805,6 +852,10 @@ namespace smil
         return nbr_subgraphs;
    }
 
+   /**
+    *  Over Segmentation Correction
+    *
+    */
    template <class labelT, class T>
    size_t stochasticFlatZones (const Image<labelT>& primary, const Image<T>& gradient, Image<labelT> &out, const size_t& n_seeds, const double &t0, const StrElt& s)
    {
@@ -849,6 +900,10 @@ namespace smil
         return nbr_subgraphs;
    }
 
+   /**
+    *  Over Segmentation Correction
+    *
+    */
    // Parallel
    template <class labelT, class T>
    size_t overSegmentationCorrection (const Image<labelT>& primary, const Image<T>& gradient, Image<labelT> &out, const size_t& n_seeds, const double& r0, const StrElt& s)
@@ -909,7 +964,7 @@ namespace smil
         applyThreshold (primary, labels, out);
         return nbr_subgraphs;
    }
-
+/** }@*/
 }
 
 #endif //_D_STOCHASTIC_CHABARDES_HPP_
