fb233f553bb91d099d7776c5159dba7d41eb969e
Jose-Marcio Martins da Cruz <Jose-Marcio.Martins@mines-paristech.fr>
2020-08-14 18:45:20 +0200

    Set new develop version to 0.10.5-dev
     On branch develop
    
     Changes to be committed:
    	new file:   Base/000-Doxygen-Morpho-Group-Order.h
    	modified:   SmilVersion.txt

---
+++ Base/000-Doxygen-Morpho-Group-Order.h
@@ -0,0 +1,25 @@
+/**
+ * @defgroup Transform    Image Transformations
+ * @ingroup  Base
+
+ * @defgroup Arith        Arithmetic operations
+ * @ingroup  Base
+
+ * @defgroup Matrix       Matrix operations
+ * @ingroup  Base
+
+ * @defgroup Histogram    Histogram
+ * @ingroup  Base
+
+ * @defgroup Measures     Base measures
+ * @ingroup  Base
+
+ * @defgroup BlobMesures  Mesures on blobs
+ * @ingroup  Base
+
+ * @defgroup Convolution  Convolution
+ * @ingroup  Base
+
+ * @defgroup Draw         Draw Operations
+ * @ingroup  Base
+ */
