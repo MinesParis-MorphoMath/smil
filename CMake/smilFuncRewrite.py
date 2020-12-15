0cddb0dba0a80d2e2a19a4ab22120a04d88771bb
Jose-Marcio Martins da Cruz <Jose-Marcio.Martins@mines-paristech.fr>
2020-12-15 12:48:14 +0100

    * New module smilFuncRewrite to add renamed functions and shortcuts. * See : mk-smilFuncRewrite.py and functions-renamed.txt
     On branch feature/dict
     Changes to be committed:
    	modified:   CMake/_smil.i
    	new file:   CMake/smilFuncRewrite.py
    	modified:   CMakeLists.txt
    	new file:   dev-tools/functions-renamed.txt
    	new file:   dev-tools/mk-smilFuncRewrite.py

---
+++ CMake/smilFuncRewrite.py
@@ -0,0 +1,204 @@
+#
+# Rewrite deprecated functions names for compatibility
+#
+from smilCorePython import *
+from smilBasePython import *
+from smilIOPython import *
+from smilMorphoPython import *
+
+
+# -------------------------------
+# Functions renamed
+#
+def GeoDist(*args):
+    """
+    Function renamed. Use:
+      r = distanceGeodesic(...)
+    """
+    return distanceGeodesic(*args)
+
+def dist(*args):
+    """
+    Function renamed. Use:
+      r = distance(...)
+    """
+    return distance(*args)
+
+def dist_euclidean(*args):
+    """
+    Function renamed. Use:
+      r = distanceEuclidean(...)
+    """
+    return distanceEuclidean(*args)
+
+def geoDil(*args):
+    """
+    Function renamed. Use:
+      r = geoDilate(...)
+    """
+    return geoDilate(*args)
+
+def geoEro(*args):
+    """
+    Function renamed. Use:
+      r = geoErode(...)
+    """
+    return geoErode(*args)
+
+def gradient_HLS(*args):
+    """
+    Function renamed. Use:
+      r = gradientHLS(...)
+    """
+    return gradientHLS(*args)
+
+def gradient_LAB(*args):
+    """
+    Function renamed. Use:
+      r = gradientLAB(...)
+    """
+    return gradientLAB(*args)
+
+def hFlip(*args):
+    """
+    Function renamed. Use:
+      r = horizFlip(...)
+    """
+    return horizFlip(*args)
+
+def measAreas(*args):
+    """
+    Function renamed. Use:
+      r = blobsArea(...)
+    """
+    return blobsArea(*args)
+
+def measBarycenters(*args):
+    """
+    Function renamed. Use:
+      r = blobsBarycenter(...)
+    """
+    return blobsBarycenter(*args)
+
+def measBlobMoments(*args):
+    """
+    Function renamed. Use:
+      r = blobsMoments(...)
+    """
+    return blobsMoments(*args)
+
+def measBlobsEntropy(*args):
+    """
+    Function renamed. Use:
+      r = blobsEntropy(...)
+    """
+    return blobsEntropy(*args)
+
+def measBoundBoxes(*args):
+    """
+    Function renamed. Use:
+      r = blobsBoundBox(...)
+    """
+    return blobsBoundBox(*args)
+
+def measImageEntropy(*args):
+    """
+    Function renamed. Use:
+      r = measEntropy(...)
+    """
+    return measEntropy(*args)
+
+def measMaxVals(*args):
+    """
+    Function renamed. Use:
+      r = blobsMaxVal(...)
+    """
+    return blobsMaxVal(*args)
+
+def measMeanVals(*args):
+    """
+    Function renamed. Use:
+      r = blobsMeanVal(...)
+    """
+    return blobsMeanVal(*args)
+
+def measMedianVal(*args):
+    """
+    Function renamed. Use:
+      r = medianVal(...)
+    """
+    return medianVal(*args)
+
+def measMedianVals(*args):
+    """
+    Function renamed. Use:
+      r = blobsMedianVal(...)
+    """
+    return blobsMedianVal(*args)
+
+def measMinVals(*args):
+    """
+    Function renamed. Use:
+      r = blobsMinVal(...)
+    """
+    return blobsMinVal(*args)
+
+def measModeVal(*args):
+    """
+    Function renamed. Use:
+      r = modeVal(...)
+    """
+    return modeVal(*args)
+
+def measModeVals(*args):
+    """
+    Function renamed. Use:
+      r = blobsModeVal(...)
+    """
+    return blobsModeVal(*args)
+
+def measRangeVals(*args):
+    """
+    Function renamed. Use:
+      r = blobsRangeVal(...)
+    """
+    return blobsRangeVal(*args)
+
+def measVolumes(*args):
+    """
+    Function renamed. Use:
+      r = blobsVolume(...)
+    """
+    return blobsVolume(*args)
+
+def vFlip(*args):
+    """
+    Function renamed. Use:
+      r = vertFlip(...)
+    """
+    return vertFlip(*args)
+
+def valueLists(*args):
+    """
+    Function renamed. Use:
+      r = blobsValueList(...)
+    """
+    return blobsValueList(*args)
+
+# -------------------------------
+# Shortcuts
+#
+def getDefaultSE(*args):
+    """
+    Shortcut
+      r = Morpho.getDefaultSE(...)
+    """
+    return Morpho.getDefaultSE(*args)
+
+def setDefaultSE(*args):
+    """
+    Shortcut
+      r = Morpho.setDefaultSE(...)
+    """
+    return Morpho.setDefaultSE(*args)
+
