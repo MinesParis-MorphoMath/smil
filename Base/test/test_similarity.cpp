cd0c5bd729841934b03ba77ac4b2baa626532836
Jose-Marcio Martins da Cruz <Jose-Marcio.Martins@mines-paristech.fr>
2021-03-22 16:29:36 +0100

    * Bug on the image similarity test - not correctly initialized.
     On branch develop
    
     Changes to be committed:
    	modified:   Base/test/test_similarity.cpp

--- Base/test/test_similarity.cpp
+++ Base/test/test_similarity.cpp
@@ -219,10 +219,12 @@ int main(void)
   m = dim/8;
   M = 5*dim/8;
 
+  fill(imGt, UINT8(0));
   for (off_t j = m; j < M; j++)
     for (off_t i = m; i < M; i++)
       imGt.setPixel(i, j, 255);
 
+  fill(imIn, UINT8(0));
   m = 2*dim/8;
   M = 7*dim/8;
   for (off_t j = m; j < M; j++)
