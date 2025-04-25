a468b57b8e86192f57bd8e6b50b1cf782b4fb518
Pierre Guillou <pierre.guillou@minesparis.psl.eu>
2025-04-25 17:44:25 +0200

    [python] Fix syntax errors for Ruff format

--- doc/demos/python/example-default-se.py
+++ doc/demos/python/example-default-se.py
@@ -1,11 +1,10 @@
-
 import smilPython as sp
 
 im = sp.Image("lena.png")
 
 # set new default StrElt and save old one
 saveSE = sp.Morpho.getDefaultSE()
-sp.Morpho.setDefaultSE(sp.SquSE))
+sp.Morpho.setDefaultSE(sp.SquSE)
 
 # do something
 imd = sp.Image(im)
@@ -13,4 +12,3 @@ sp.dilate(im, imd, 3)
 
 # restore old default StrElt
 sp.Morpho.setDefaultSE(saveSE)
-
--- doc/demos/python/numpy_sp2np.py
+++ doc/demos/python/numpy_sp2np.py
@@ -1,9 +1,8 @@
-
 import smilPython as sm
 import numpy as np
 
 # read a PNG image file (8 bits gray image)
-file = "my-image.png")
+file = "my-image.png"
 img = sm.Image(file)
 
 # show the image
@@ -15,8 +14,7 @@ p = img.getNumpyArray()
 # let's threshold the image
 t = 127
 p[p >= t] = 255
-p[p <  t] = 0
+p[p < t] = 0
 
 # Call the "modified" method in order to update the viewer content
 img.modified()
-
